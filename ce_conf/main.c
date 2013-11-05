0af4455602fe780ea8b79aa7a498fb6a04a72189
atarijookie <atarijookie@0d2ca917-fb58-4453-bb26-a698c01b0940>
2013-11-05 08:58:33 +0000

    vkey and key now translated to single byte, acsi communcation and commands added
    git-svn-id: https://subversion.assembla.com/svn/atarijookie/trunk@142 0d2ca917-fb58-4453-bb26-a698c01b0940

--- ce_conf/main.c
+++ ce_conf/main.c
@@ -6,18 +6,29 @@
 #include <stdlib.h>
 
 #include "acsi.h"
+#include "keys.h"
 /*--------------------------------------------------*/
-BYTE CE_ReadRunningFW(BYTE ACSI_id, BYTE *buffer);
 
-void getScreenStream(BYTE homeScreen);			/* if homeScreen=1, gets homescreen; for 0 it gets current screen */
+void showHomeScreen(void);
 void sendKeyDown(BYTE vkey, BYTE key);
+BYTE atariKeysToSingleByte(BYTE vkey, BYTE key);
+BYTE ce_identify(BYTE ACSI_id);
 /*--------------------------------------------------*/
 BYTE      deviceID;
 
 BYTE myBuffer[520];
 BYTE *pBuffer;
+
+#define HOSTMOD_CONFIG				1
+#define HOSTMOD_LINUX_TERMINAL		2
+#define HOSTMOD_TRANSLATED_DISK		3
+#define HOSTMOD_NETWORK_ADAPTER		4
+
+#define CFG_CMD_IDENTIFY			0
+#define CFG_CMD_KEYDOWN				1
+#define CFG_CMD_GO_HOME				0xff
 /*--------------------------------------------------*/
-void main(void)
+int main(void)
 {
   DWORD scancode;
   BYTE key, vkey, res;
@@ -43,15 +54,15 @@ void main(void)
 		for(i=0; i<8; i++) {
 			printf("%d", i);
       
-			res = CE_ReadRunningFW(i, pBuffer);      /* try to read FW name */
+			res = ce_identify(i);      					/* try to read the IDENTITY string */
       
-			if(res == 1) {                           	/* if found the US */
+			if(res == 1) {                           	/* if found the CosmosEx */
 				deviceID = i;                     		/* store the ACSI ID of device */
 				break;
 			}
 		}
   
-		if(res == 1) {                             /* if found, break */
+		if(res == 1) {                             		/* if found, break */
 			break;
 		}
       
@@ -59,13 +70,13 @@ void main(void)
 		key = Cnecin();
     
 		if(key == 'Q' || key=='q') {
-			return;
+			return 0;
 		}
 	}
   
-	printf("\n\nCosmosEx ACSI ID: %d\nFirmWare: %s", (int) deviceID, (char *)pBuffer);
+	printf("\n\nCosmosEx ACSI ID: %d\n", (int) deviceID);
 	/* ----------------- */
-	getScreenStream(1);							/* get the home screen */
+	showHomeScreen();							/* get the home screen */
 	
 	/* use Ctrl + C to quit */
 	timePrev = Tgettime();
@@ -79,7 +90,7 @@ void main(void)
 			if((timeNow - timePrev) > 0) {		/* check if time changed (2 seconds passed) */
 				timePrev = timeNow;
 				
-				getScreenStream(0);				/* display a new stream (if something changed) */
+				sendKeyDown(0, 0);				/* display a new stream (if something changed) */
 			}
 			
 			continue;							/* try again */
@@ -91,39 +102,111 @@ void main(void)
 		key		=  scancode			& 0xff;
 
 		sendKeyDown(vkey, key);					/* send this key to device */
-		getScreenStream(0);						/* and display the screen */
 	}
 }
 /*--------------------------------------------------*/
 void sendKeyDown(BYTE vkey, BYTE key)
 {
-	/* todo: send a command with these two bytes to CosmosEx device */
+	WORD res;
+	BYTE cmd[] = {0, 'C', 'E', HOSTMOD_CONFIG, CFG_CMD_KEYDOWN, 0};
 	
+	cmd[0] = (deviceID << 5); 						/* cmd[0] = ACSI_id + TEST UNIT READY (0)	*/
+	cmd[5] = atariKeysToSingleByte(vkey, key);		/* store the pressed key to cmd[5] */
+  
+	memset(pBuffer, 0, 512);               			/* clear the buffer */
+  
+	res = acsi_cmd(1, cmd, 6, pBuffer);    			/* issue the KEYDOWN command and show the screen stream */
+    
+	if(res != OK) {									/* if failed, return FALSE */
+		printf("sendKeyDown failed!\n");
+		return;
+	}
+	
+	Cconws((char *) pBuffer);						/* now display the buffer */
 }
 /*--------------------------------------------------*/
-void getScreenStream(BYTE homeScreen)			/* if homeScreen=1, gets homescreen; for 0 it gets current screen */
+void showHomeScreen(void)							
 {
-	/* todo: send command over ACSI and receive screen stream to pBuffer */
+	WORD res;
+	BYTE cmd[] = {0, 'C', 'E', HOSTMOD_CONFIG, CFG_CMD_GO_HOME, 0};
 	
+	cmd[0] = (deviceID << 5); 						/* cmd[0] = ACSI_id + TEST UNIT READY (0)	*/
+	memset(pBuffer, 0, 512);               			/* clear the buffer */
+  
+	res = acsi_cmd(1, cmd, 6, pBuffer);    			/* issue the GO_HOME command and show the screen stream */
+    
+	if(res != OK) {									/* if failed, return FALSE */
+		printf("showHomeScreen failed!\n");
+		return;
+	}
 	
-	/* now display the buffer */
-	Cconws((char *) pBuffer);	
+	Cconws((char *) pBuffer);						/* now display the buffer */
 }
 /*--------------------------------------------------*/
-BYTE CE_ReadRunningFW(BYTE ACSI_id, BYTE *buffer)
+BYTE ce_identify(BYTE ACSI_id)
 {
   WORD res;
-  BYTE cmd[] = {0x1f, 0x20, 'U', 'S', 'C', 'u', 'r', 'n', 't', 'F', 'W'};
+  BYTE cmd[] = {0, 'C', 'E', HOSTMOD_CONFIG, CFG_CMD_IDENTIFY, 0};
   
-  cmd[0] = 0x1f | (ACSI_id << 5);  
-  memset(buffer, 0, 512);               /* clear the buffer */
+  cmd[0] = (ACSI_id << 5); 				/* cmd[0] = ACSI_id + TEST UNIT READY (0)	*/
+  memset(pBuffer, 0, 512);              /* clear the buffer */
   
-  res = acsi_cmd(1, cmd, 11, buffer);         /* read name and version of current FW */
+  res = acsi_cmd(1, cmd, 6, pBuffer);   /* issue the identify command and check the result */
     
   if(res != OK)                         /* if failed, return FALSE */
     return 0;
     
+  if(strncmp((char *) pBuffer, "CosmosEx config console", 23) != 0) {		/* the identity string doesn't match? */
+	return 0;
+  }
+	
   return 1;                             /* success */
+}
+/*--------------------------------------------------*/
+BYTE atariKeysToSingleByte(BYTE vkey, BYTE key)
+{
+	WORD vkeyKey;
+
+	if(key >= 32 && key < 127) {		/* printable ASCII key? just return it */
+		return key;
+	}
+	
+	if(key == 0) {						/* will this be some non-ASCII key? convert it */
+		switch(vkey) {
+			case 0x48: return KEY_UP;
+			case 0x50: return KEY_DOWN;
+			case 0x4b: return KEY_LEFT;
+			case 0x4d: return KEY_RIGHT;
+			case 0x52: return KEY_INSERT;
+			case 0x47: return KEY_HOME;
+			case 0x62: return KEY_HELP;
+			case 0x61: return KEY_UNDO;
+			case 0x3b: return KEY_F1;
+			case 0x3c: return KEY_F2;
+			case 0x3d: return KEY_F3;
+			case 0x3e: return KEY_F4;
+			case 0x3f: return KEY_F5;
+			case 0x40: return KEY_F6;
+			case 0x41: return KEY_F7;
+			case 0x42: return KEY_F8;
+			case 0x43: return KEY_F9;
+			case 0x44: return KEY_F10;
+			default: return 0;			/* unknown key */
+		}
+	}
+	
+	vkeyKey = (((WORD) vkey) << 8) | ((WORD) key);		/* create a WORD with vkey and key together */
+	
+	switch(vkeyKey) {					/* some other no-ASCII key, but check with vkey too */
+		case 0x011b: return KEY_ESC;
+		case 0x537f: return KEY_DELETE;
+		case 0x0e08: return KEY_BACKSP;
+		case 0x0f09: return KEY_TAB;
+		case 0x1c0d: return KEY_ENTER;
+		case 0x720d: return KEY_ENTER;
+	}
+
+	return 0;							/* unknown key */
 }
 /*--------------------------------------------------*/
 
