6cbc1d43df87a409f3013431aa1cd413ac8765ae
atarijookie <atarijookie@0d2ca917-fb58-4453-bb26-a698c01b0940>
2014-01-09 08:23:23 +0000

    reset signal from MCU added to VHDL code, pins rearranged for the final HW
    git-svn-id: https://subversion.assembla.com/svn/atarijookie/trunk@249 0d2ca917-fb58-4453-bb26-a698c01b0940

--- acsi_vdhl/main.vhd
+++ acsi_vdhl/main.vhd
@@ -5,19 +5,20 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity main is
     Port ( 
-    		-- signals connected to MCU
-    		 PIO   : in std_logic;			-- on rising edge will put INT to L
-           DMA   : in std_logic;			-- on rising edge will put DRQ to L
-           RnW   : in std_logic;			-- defines data direction (1: DATA1 <- DATA2,  0: DATA1 -> DATA2)
-           CMD   : out std_logic;			-- this is combination of CS and A1, will go low on 1st cmd byte from ACSI port
+		-- signals connected to MCU
+			PIO   		: in std_logic;			-- on rising edge will put INT to L
+			DMA   		: in std_logic;			-- on rising edge will put DRQ to L
+			RnW   		: in std_logic;			-- defines data direction (1: DATA1 <- DATA2,  0: DATA1 -> DATA2)
+			CMD			: out std_logic;			-- this is combination of CS and A1, will go low on 1st cmd byte from ACSI port
+			reset_hans	: in std_logic;			-- this is the signal which resets Hans, and it will reset this CPLD too to init it
 
 		-- signals connected to ACSI port
-           INT   : out std_logic;
-           DRQ   : out std_logic;
-           CS    : in std_logic;
-           A1    : in std_logic;
-           ACK   : in std_logic;
-		 RESET : in std_logic;
+			INT   : out std_logic;
+			DRQ   : out std_logic;
+			CS    : in std_logic;
+			A1    : in std_logic;
+			ACK   : in std_logic;
+			RESET : in std_logic;
 
 		-- DATA1 is connected to ACSI port, DATA2 is data latched on CS and ACK and connected to MCU
            DATA1 : inout std_logic_vector(7 downto 0);
@@ -25,10 +26,10 @@ entity main is
 
 
 		-- the following is 2-to-1 Multiplexer for connecting both MCUs to single RX pin (used for FW update)
-		 TXSEL1n2: in std_logic;			-- TX select -    1: TX_out <- TX1_in,    0: TX_out <- TX2_in 
-		 TX1_in  : in  std_logic;		-- TX from first  MCU
-		 TX2_in  : in  std_logic;		-- TX from second MCU
-		 TX_out  : out std_logic			-- muxed TX
+			TXSEL1n2: in std_logic;			-- TX select -    1: TX_out <- TX1_in,    0: TX_out <- TX2_in 
+			TX1_in  : in  std_logic;		-- TX from first  MCU
+			TX2_in  : in  std_logic;		-- TX from second MCU
+			TX_out  : out std_logic			-- muxed TX
 		) ;
 end main;
 
@@ -42,9 +43,9 @@ begin
 		   
 	-- D flip-flop with asynchronous reset 
 	-- pull INT low after rising edge of PIO, let it in hi-Z after reset or low on CS
-	PIOrequest: process(PIO, latchClock, RESET) is				
+	PIOrequest: process(PIO, latchClock, RESET, reset_hans) is				
 	begin
-		if ((latchClock = '0') or (RESET = '0')) then
+		if ((latchClock = '0') or (RESET = '0') or (reset_hans = '0')) then
 			INTstate <= '1';
 		elsif (rising_edge(PIO)) then
 			INTstate <= '0';
@@ -53,9 +54,9 @@ begin
 
 	-- D flip-flop with asynchronous reset 
 	-- pull DRQ low after rising edge of DMA, let it in hi-Z after reset or low on ACK
-	DMArequest: process(DMA, latchClock, RESET) is				
+	DMArequest: process(DMA, latchClock, RESET, reset_hans) is				
 	begin
-		if ((latchClock = '0') or (RESET = '0')) then
+		if ((latchClock = '0') or (RESET = '0') or (reset_hans = '0')) then
 			DRQstate <= '1';
 		elsif (rising_edge(DMA)) then
 			DRQstate <= '0';
