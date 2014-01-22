5b75f875e2e99ec7e1f4a3125465f6a0acae0fe6
atarijookie <atarijookie@0d2ca917-fb58-4453-bb26-a698c01b0940>
2014-01-22 06:53:52 +0000

    showing of memory usage of the driver added to convertor
    git-svn-id: https://subversion.assembla.com/svn/atarijookie/trunk@266 0d2ca917-fb58-4453-bb26-a698c01b0940

--- cosmosex_st_bootsector/convertor.cpp
+++ cosmosex_st_bootsector/convertor.cpp
@@ -2,8 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define BYTE    unsigned char
 #define WORD    unsigned short
+#define DWORD   unsigned int
+
 WORD swapNibbles(WORD val);
+
+void checkCeddSize(void);
+DWORD getValue(BYTE *p);
 
 int main(int argc, char *argv[])
 {
@@ -44,15 +50,17 @@ int main(int argc, char *argv[])
     }
 
     WORD cs = 0x1234 - sum;
+    sum = sum & 0xffff;
 
-    printf("sum %04x, check-sum is %04x, check is %04x", sum, cs, (cs + sum));
+
+    printf("sum %04x, check-sum is %04x, check is %04x", sum, cs, (cs + sum) & 0xffff);
 
     bfr[510] = cs >> 8;         // store the check sum
     bfr[511] = cs;
 
     //---------------------------------------
     // now write it to file
-    FILE *bs = fopen("ceddboot.bin", "wb");
+    FILE *bs = fopen("ceddboot.001", "wb");
 
     if(!bs) {
         printf("\nCould not open OUTPUT file!\n");
@@ -63,9 +71,54 @@ int main(int argc, char *argv[])
     fwrite(bfr, 1, 512, bs);
 
     fclose(bs);
+    //---------------------------------------
+    checkCeddSize();
 
     getchar();
     return 0;
+}
+
+void checkCeddSize(void)
+{
+    FILE *f = fopen("ceddboot.002", "rb");
+
+    if(!f) {
+        printf("\nCould not open ceddboot.002 file!\n");
+        getchar();
+        return;
+    }
+
+    char bfr[20];
+    fread(bfr, 1, 20, f);
+
+    fclose(f);
+
+    DWORD tsize, dsize, bsize;
+
+    tsize = getValue((BYTE *)(bfr + 2));
+    dsize = getValue((BYTE *)(bfr + 6));
+    bsize = getValue((BYTE *)(bfr + 10));
+
+    printf("\n\nCEDD driver \n");
+    printf("text  size: %d\n", tsize);
+    printf("data  size: %d\n", dsize);
+    printf("bss   size: %d\n", bsize);
+    printf("TOTAL size: %d\n", tsize + dsize + bsize);
+}
+
+DWORD getValue(BYTE *p)
+{
+    DWORD val = 0;
+
+    val = ((DWORD) p[0]);
+    val = val << 8;
+    val = val  | ((DWORD) p[1]);
+    val = val << 8;
+    val = val  | ((DWORD) p[2]);
+    val = val << 8;
+    val = val  | ((DWORD) p[3]);
+
+    return val;
 }
 
 WORD swapNibbles(WORD val)
