5b4fa9b462e10f14552c2054f3a62f6c8c861287
atarijookie <atarijookie@0d2ca917-fb58-4453-bb26-a698c01b0940>
2014-03-07 13:53:58 +0000

    added watchdog / update script
    git-svn-id: https://subversion.assembla.com/svn/atarijookie/trunk@363 0d2ca917-fb58-4453-bb26-a698c01b0940

---
+++ cosmosex/other/cesuper.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+while :
+do
+
+    #try to get pid of process
+    pid=$(pidof cosmosex)
+
+    if [ -z "$pid" ]            # if pid of process is empty, process not running
+    then
+
+        # app is not running - either we want to do update, or it crashed
+
+        if [ -a /ce/update/doupdate.sh ]        # if update file exists, we do the update
+        then
+            echo "doing update"
+
+            /ce/update/doupdate.sh              # execute the update script, wait for finish
+            rm -f /ce/update/doupdate.sh        # delete the update script
+        fi
+
+        # if we got here, there was an update or crash, so run the app!
+        echo "restarting app"
+        /ce/app/cosmosex &                      # run the application on the background
+
+    else                        # if pid of process is not empty, process is running
+
+        # app is running, just sleep and check status later
+        echo "app is running, sleeping"
+
+    fi
+
+    sleep 5
+
+done
+
