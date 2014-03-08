b0da05f2bcaaa6e0e344ceeaf3849d43871ca9c8
atarijookie <atarijookie@0d2ca917-fb58-4453-bb26-a698c01b0940>
2014-03-08 14:34:33 +0000

    first time installation script added
    git-svn-id: https://subversion.assembla.com/svn/atarijookie/trunk@366 0d2ca917-fb58-4453-bb26-a698c01b0940

---
+++ cosmosex/other/first.sh
@@ -0,0 +1,32 @@
+# delete and create dir for the cosmosex
+sudo rm -rf /ce
+sudo mkdir /ce
+sudo chmod 777 /ce
+mkdir /ce/update
+cd /ce/update
+
+# download update list and flash tools
+wget http://joo.kie.sk/cosmosex/update/updatelist.csv
+wget http://joo.kie.sk/cosmosex/update/flash_stm32
+wget http://joo.kie.sk/cosmosex/update/flash_xilinx
+
+# change permissions to be able to run this
+chmod 755 flash_stm32
+chmod 755 flash_xilinx
+
+# get only column with urls from csv and wget it here
+sed 's/[^,]*,[^,]*,\([^,]*\).*/\1/' updatelist.csv | while read line; 
+do
+	wget $line
+done
+
+# now flash all the components to chips
+sudo ./flash_xilinx xilinx.xsvf
+sudo ./flash_stm32 -x -w hans.hex /dev/ttyAMA0
+sudo ./flash_stm32 -y -w franz.hex /dev/ttyAMA0
+
+cd /ce
+mv /ce/update/app.zip /ce
+unzip app.zip
+chmod 777 /ce/app/cosmosex
+
