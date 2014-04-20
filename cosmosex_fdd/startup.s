aa03205ac40e407fe330cc455446315c900f7b60
atarijookie <atarijookie@0d2ca917-fb58-4453-bb26-a698c01b0940>
2014-04-20 00:26:59 +0000

    makefile changed and thus crash fixed, wtf gcc??
    git-svn-id: https://subversion.assembla.com/svn/atarijookie/trunk@416 0d2ca917-fb58-4453-bb26-a698c01b0940

--- cosmosex_fdd/startup.s
+++ cosmosex_fdd/startup.s
@@ -1,19 +1,10 @@
 	.globl	_main
 	.globl	__base
 	.globl	___main
-	.globl  __runFromBootsector
 
 | ------------------------------------------------------	
 	.text
 | ------------------------------------------------------
-	move.l	-4(sp),d0				| address to basepage
-
-	cmp.l	#0x12345678, d0			| if found a magic number then we're called from bootsector?
-	beq		fromBootsector			| skip Mshrink, terminate with rts
-
-| the following code should run when driver was run from TOS (not bootsector)
-	move.l	#0,__runFromBootsector	| mark that the app was run from TOS 
-
 	move.l	4(sp),a5				| address to basepage
 	move.l	0x0c(a5),d0				| length of text segment
 	add.l	0x14(a5),d0				| length of data segment
@@ -43,16 +34,8 @@ ___main:
 	
 | the following code should run when driver was run from bootsector (not TOS)
 	
-fromBootsector:
-	move.l	#1,__runFromBootsector	| mark that the app was run from bootsector 
-
-	jsr	_main
-	
-	rts
-
 | ------------------------------------------------------
 	.bss
 | ------------------------------------------------------	
 __base:					.ds.l	1
-__runFromBootsector:	.ds.l	1
 
