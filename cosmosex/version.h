7feb3401333f6282534d3190892781bc254661b5
atarijookie <atarijookie@0d2ca917-fb58-4453-bb26-a698c01b0940>
2014-06-29 03:16:59 +0000

    app version updated
    git-svn-id: https://subversion.assembla.com/svn/atarijookie/trunk@501 0d2ca917-fb58-4453-bb26-a698c01b0940

--- cosmosex/version.h
+++ cosmosex/version.h
@@ -5,7 +5,7 @@
 #include "datatypes.h"
 
 #define XILINX_VERSION_FILE		"/ce/update/xilinx_current.txt"
-#define APP_VERSION         	"2014-06-28"
+#define APP_VERSION         	"2014-06-29"
 
 class Version
 {
