ce824cbe42729189300d4ed786edb3e01462868a
atarijookie <atarijookie@0d2ca917-fb58-4453-bb26-a698c01b0940>
2014-10-10 14:00:08 +0000

    just random changes
    git-svn-id: https://subversion.assembla.com/svn/atarijookie/trunk@642 0d2ca917-fb58-4453-bb26-a698c01b0940

--- cosmosex_fakesting/globdefs.h
+++ cosmosex_fakesting/globdefs.h
@@ -425,41 +425,40 @@ typedef  struct pnta {
  *   Error return values.
  */
 
-#define  E_NORMAL         0     /* No error occured ...                     */
-#define  E_OBUFFULL      -1     /* Output buffer is full                    */
-#define  E_NODATA        -2     /* No data available                        */
-#define  E_EOF           -3     /* EOF from remote                          */
-#define  E_RRESET        -4     /* Reset received from remote               */
-#define  E_UA            -5     /* Unacceptable packet received, reset      */
-#define  E_NOMEM         -6     /* Something failed due to lack of memory   */
-#define  E_REFUSE        -7     /* Connection refused by remote             */
-#define  E_BADSYN        -8     /* A SYN was received in the window         */
-#define  E_BADHANDLE     -9     /* Bad connection handle used.              */
-#define  E_LISTEN        -10    /* The connection is in LISTEN state        */
-#define  E_NOCCB         -11    /* No free CCB's available                  */
-#define  E_NOCONNECTION  -12    /* No connection matches this packet (TCP)  */
-#define  E_CONNECTFAIL   -13    /* Failure to connect to remote port (TCP)  */
-#define  E_BADCLOSE      -14    /* Invalid TCP_close() requested            */
-#define  E_USERTIMEOUT   -15    /* A user function timed out                */
-#define  E_CNTIMEOUT     -16    /* A connection timed out                   */
-#define  E_CANTRESOLVE   -17    /* Can't resolve the hostname               */
-#define  E_BADDNAME      -18    /* Domain name or dotted dec. bad format    */
-#define  E_LOSTCARRIER   -19    /* The modem disconnected                   */
-#define  E_NOHOSTNAME    -20    /* Hostname does not exist                  */
-#define  E_DNSWORKLIMIT  -21    /* Resolver Work limit reached              */
-#define  E_NONAMESERVER  -22    /* No nameservers could be found for query  */
-#define  E_DNSBADFORMAT  -23    /* Bad format of DS query                   */
-#define  E_UNREACHABLE   -24    /* Destination unreachable                  */
-#define  E_DNSNOADDR     -25    /* No address records exist for host        */
-#define  E_NOROUTINE     -26    /* Routine unavailable                      */
-#define  E_LOCKED        -27    /* Locked by another application            */
-#define  E_FRAGMENT      -28    /* Error during fragmentation               */
-#define  E_TTLEXCEED     -29    /* Time To Live of an IP packet exceeded    */
-#define  E_PARAMETER     -30    /* Problem with a parameter                 */
-#define  E_BIGBUF        -31    /* Input buffer is too small for data       */
-#define  E_FNAVAIL       -32    /* Function not available                   */
-#define  E_LASTERROR      32    /* ABS of last error code in this list      */
-
+#define  E_NORMAL         0     /* No error occured ...                     0x00 */
+#define  E_OBUFFULL      -1     /* Output buffer is full                    0xff */
+#define  E_NODATA        -2     /* No data available                        0xfe */
+#define  E_EOF           -3     /* EOF from remote                          0xfd */
+#define  E_RRESET        -4     /* Reset received from remote               0xfc */
+#define  E_UA            -5     /* Unacceptable packet received, reset      0xfb */
+#define  E_NOMEM         -6     /* Something failed due to lack of memory   0xfa */
+#define  E_REFUSE        -7     /* Connection refused by remote             0xf9 */
+#define  E_BADSYN        -8     /* A SYN was received in the window         0xf8 */
+#define  E_BADHANDLE     -9     /* Bad connection handle used.              0xf7 */
+#define  E_LISTEN        -10    /* The connection is in LISTEN state        0xf6 */
+#define  E_NOCCB         -11    /* No free CCB's available                  0xf5 */
+#define  E_NOCONNECTION  -12    /* No connection matches this packet (TCP)  0xf4 */
+#define  E_CONNECTFAIL   -13    /* Failure to connect to remote port (TCP)  0xf3 */
+#define  E_BADCLOSE      -14    /* Invalid TCP_close() requested            0xf2 */
+#define  E_USERTIMEOUT   -15    /* A user function timed out                0xf1 */
+#define  E_CNTIMEOUT     -16    /* A connection timed out                   0xf0 */
+#define  E_CANTRESOLVE   -17    /* Can't resolve the hostname               0xef */
+#define  E_BADDNAME      -18    /* Domain name or dotted dec. bad format    0xee */
+#define  E_LOSTCARRIER   -19    /* The modem disconnected                   0xed */
+#define  E_NOHOSTNAME    -20    /* Hostname does not exist                  0xec */
+#define  E_DNSWORKLIMIT  -21    /* Resolver Work limit reached              0xeb */
+#define  E_NONAMESERVER  -22    /* No nameservers could be found for query  0xea */
+#define  E_DNSBADFORMAT  -23    /* Bad format of DS query                   0xe9 */
+#define  E_UNREACHABLE   -24    /* Destination unreachable                  0xe8 */
+#define  E_DNSNOADDR     -25    /* No address records exist for host        0xe7 */
+#define  E_NOROUTINE     -26    /* Routine unavailable                      0xe6 */
+#define  E_LOCKED        -27    /* Locked by another application            0xe5 */
+#define  E_FRAGMENT      -28    /* Error during fragmentation               0xe4 */
+#define  E_TTLEXCEED     -29    /* Time To Live of an IP packet exceeded    0xe3 */
+#define  E_PARAMETER     -30    /* Problem with a parameter                 0xe2 */
+#define  E_BIGBUF        -31    /* Input buffer is too small for data       0xe1 */
+#define  E_FNAVAIL       -32    /* Function not available                   0xe0 */
+#define  E_LASTERROR      32    /* ABS of last error code in this list      0xe0 */
 
 
 /*--------------------------------------------------------------------------*/
