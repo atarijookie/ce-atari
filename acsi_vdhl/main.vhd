53b8f2a4d23e8ad995b7392dc590c0e63afa90e5
atarijookie <atarijookie@0d2ca917-fb58-4453-bb26-a698c01b0940>
2015-03-01 05:15:05 +0000

    added IDENTIFY state to be able to read Xilinx / HW version
    git-svn-id: https://subversion.assembla.com/svn/atarijookie/trunk@796 0d2ca917-fb58-4453-bb26-a698c01b0940

--- acsi_vdhl/main.vhd
+++ acsi_vdhl/main.vhd
@@ -39,27 +39,30 @@ architecture Behavioral of main is
     signal DATA1latch: std_logic_vector(7 downto 0);
     signal latchClock: std_logic;
     signal resetCombo: std_logic;
+    signal identify  : std_logic;
 
 begin
            
     -- D flip-flop with asynchronous reset 
     -- pull INT low after rising edge of PIO, let it in hi-Z after reset or low on CS
-    PIOrequest: process(PIO, latchClock, RESET, reset_hans) is
+    -- DMA pin has to be low when toggling PIO hi and low
+    PIOrequest: process(PIO, DMA, latchClock, RESET, reset_hans) is
     begin
         if ((latchClock = '0') or (RESET = '0') or (reset_hans = '0')) then
             INTstate <= '1';
-        elsif (rising_edge(PIO)) then
+        elsif (rising_edge(PIO) and (DMA = '0')) then
             INTstate <= '0';
         end if;
     end process;
 
     -- D flip-flop with asynchronous reset 
     -- pull DRQ low after rising edge of DMA, let it in hi-Z after reset or low on ACK
-    DMArequest: process(DMA, latchClock, RESET, reset_hans) is
+    -- PIO pin has to be low when toggling DMA hi and low
+    DMArequest: process(DMA, PIO, latchClock, RESET, reset_hans) is
     begin
         if ((latchClock = '0') or (RESET = '0') or (reset_hans = '0')) then
             DRQstate <= '1';
-        elsif (rising_edge(DMA)) then
+        elsif (rising_edge(DMA) and (PIO = '0')) then
             DRQstate <= '0';
         end if;
     end process;
@@ -76,6 +79,8 @@ begin
     latchClock <= CS and ACK;                           -- need this only to be able to react on falling edge of both CS and ACK
     resetCombo <= RESET and reset_hans;                 -- when at least one of those 2 reset signals is low, the result is low
 
+    identify   <= PIO and DMA and TXSEL1n2;			 -- when TXSEL1n2 selects Franz (='1') and you have PIO and DMA pins high, then you can read the identification byte from DATA2
+
     INT <= '0' when INTstate='0' else 'Z';              -- INT - pull to L, otherwise hi-Z
     DRQ <= '0' when DRQstate='0' else 'Z';              -- DRQ - pull to L, otherwise hi-Z
     CMD <= CS or A1 or (not RESET);                     -- CMD - falling edge here will tell that CS with A1 low has been found (and ACSI RESET has to be high at that time)
@@ -87,6 +92,7 @@ begin
 
     -- DATA2 is connected to Hans (STM32 mcu), data goes out when going from ST to MCU (WRITE operation)
     DATA2 <=    "ZZZZZ0ZZ"  when TXSEL1n2='0'    else   -- when TXSEL1n2 selects Hans, we're writing to Hans's flash, we need bit DATA2.2 (bit #2) to be 0 (it's BOOT1 bit on STM32 MCU)
+                "00010001"  when identify='1'    else   -- when identify condition met, this identifies the XILINX and HW revision (0001 - HW rev 1, 0001 - ACSI version)
                 DATA1latch  when RnW='0'         else   -- when set in WRITE direction, output latched DATA1 to DATA2 
                 "ZZZZZZZZ";                             -- otherwise don't drive this
 
