3f89c9a430f62dd2bd31ebcbe46b0cfaa40b88cc
atarijookie <atarijookie@0d2ca917-fb58-4453-bb26-a698c01b0940>
2015-05-12 07:15:19 +0000

    added tests for Fdelete(), Ddelete(), Frename()
    git-svn-id: https://subversion.assembla.com/svn/atarijookie/trunk@870 0d2ca917-fb58-4453-bb26-a698c01b0940

--- filesystem_test/test04.c
+++ filesystem_test/test04.c
@@ -12,6 +12,9 @@ extern int drive;
 void test040x(void);
 void test041x(void);
 void test043x(void);
+void test044x(void);
+void test045x(void);
+void test046x(void);
 
 BYTE filenameExists(char *filename);
 
@@ -30,12 +33,14 @@ void test04(void)
     Dsetdrv(drive);
     (void) Dcreate("\\TEST04");
     Dsetpath("\\TEST04");
-    
+
     test040x();
     test041x();
     test043x();
-
- 
+    test044x();
+    test045x();
+    test046x();
+    
     Ddelete("\\TEST04");
     out_s("");
 }
@@ -96,6 +101,114 @@ void test043x(void)
     testSingleInvalidChar(0x0433, "\\", 0xffde);        // steem returns 0xffdc (access denied), native drive returns 0xffde (folder not found)
 }
 
+void test044x(void)
+{
+    WORD res1, res2, ok;
+
+    (void) Dcreate("TOBEDELE");
+    res1 = Ddelete("TOBEDELE");
+    res2 = filenameExists("TOBEDELE");
+    (res1 == 0 && res2 == 0) ? (ok = 1) : (ok = 0);
+    out_tr_bw(0x0441, "Ddelete - delete empty dir", ok, res1);
+
+    (void) Dcreate("DELETEME");
+    int f = Fcreate("DELETEME\\SUBFILE", 0);
+    Fclose(f);
+    res1 = Ddelete("DELETEME");
+    res2 = filenameExists("DELETEME");
+    (res1 == 0xffdc && res2 == 1) ? (ok = 1) : (ok = 0);
+    out_tr_bw(0x0442, "Ddelete - delete non-empty dir", ok, res1);
+    
+    res1 = Ddelete("HOGOFOGO");
+    (res1 == 0xffde || res1 == 0xffdc) ? (ok = 1) : (ok = 0);           // native drive returns 0xffde, steem returns 0xffdc
+    out_tr_bw(0x0443, "Ddelete - delete non-existing dir", ok, res1);
+
+    res1 = Ddelete("DELETEME\\SUBFILE");
+    (res1 == 0xffde || res1 == 0xffdc) ? (ok = 1) : (ok = 0);           // native drive returns 0xffde, steem returns 0xffdc
+    out_tr_bw(0x0444, "Ddelete - delete a file", ok, res1);
+
+    Fdelete("DELETEME\\SUBFILE");
+    Ddelete("DELETEME");
+}
+
+void test045x(void)
+{
+    WORD res1, res2, res3, res4, ok;
+
+    Fdelete("MYFILE");
+    Fdelete("NEWFILE");
+    Fdelete("NOTEXIST");
+    Fdelete("DERP*");
+    
+    int f = Fcreate("MYFILE", 0);
+    Fclose(f);
+    
+    res1 = filenameExists("MYFILE");
+    res2 = Frename(0, "MYFILE", "NEWFILE");
+    res3 = filenameExists("MYFILE");
+    res4 = filenameExists("NEWFILE");
+    ok = 0;
+    if(res1 == 1 && res2 == 0 && res3 == 0 && res4 == 1) {
+        ok = 1;
+    }
+    out_tr_bw(0x0451, "Frename - rename a existing file", ok, res2);
+
+    res1 = Frename(0, "NOTEXIST", "NEWFILE");
+    (res1 == 0xffdf) ? (ok = 1) : (ok = 0);
+    out_tr_bw(0x0452, "Frename - rename a non-existing file", ok, res1);
+    
+    res1 = Frename(0, "NEWFILE", "DERP*");
+    (res1 == 0 || res1 == 0xffdc) ? (ok = 1) : (ok = 0);           // TOS bug? Native drive returns 0, steem returns 0xffdc
+    out_tr_bw(0x0453, "Frename - rename to invalid file", ok, res1);
+    
+    Ddelete("MYDIR");
+    Ddelete("NEWDIR");
+    (void) Dcreate("MYDIR");
+    res1 = Frename(0, "MYDIR", "NEWDIR");
+    res2 = filenameExists("MYDIR");
+    res3 = filenameExists("NEWDIR");
+    (res1 == 0 && res2 == 0 && res3 == 1) ? (ok = 1) : (ok = 0);
+    out_tr_bw(0x0454, "Frename - rename a directory", ok, res1);
+
+    Ddelete("MYDIR");
+    Ddelete("NEWDIR");
+    Fdelete("MYFILE");
+    Fdelete("NEWFILE");
+    Fdelete("NOTEXIST");
+    Fdelete("DERP*");
+}
+
+void test046x(void)
+{
+    WORD res1, res2, res3, ok;
+
+    Fdelete("MYFILE");
+    Fdelete("NEWFILE");
+    Fdelete("NOTEXIST");
+    Fdelete("DERP*");
+
+    int f = Fcreate("MYFILE", 0);
+    Fclose(f);
+    res1 = filenameExists("MYFILE");
+    res2 = Fdelete("MYFILE");
+    res3 = filenameExists("MYFILE");
+    (res1 == 1 && res2 == 0 && res3 == 0) ? (ok = 1) : (ok = 0);
+    out_tr_bw(0x0461, "Fdelete - delete existing file", ok, res1);
+    
+    res1 = filenameExists("NOTEXIST");
+    res2 = Fdelete("NOTEXIST");
+    (res1 == 0 && res2 == 0xffdf) ? (ok = 1) : (ok = 0);
+    out_tr_bw(0x0462, "Fdelete - delete non-existing file", ok, res2);
+    
+    (void) Dcreate("MYDIR");    
+    res1 = filenameExists("MYDIR");
+    res2 = Fdelete("MYDIR");
+    (res1 == 1 && (res2 == 0xffdf || res2 == 0xffdc)) ? (ok = 1) : (ok = 0);        // native drive returns 0xffdf, Steem drive returns 0xffdc
+    out_tr_bw(0x0463, "Fdelete - delete a directory", ok, res2);
+    
+    Ddelete("MYDIR");
+}
+    
 void testSingleValidChar(WORD testCaseNo, char *fname)
 {
     WORD res, ok;
