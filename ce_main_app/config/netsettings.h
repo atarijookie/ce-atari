0721d177dfe92fbcde405c59847a7cd20668f1ab
atarijookie <joo@kie.sk>
2016-04-25 11:15:46 +0200

    Minor change to make floppy encoding quit faster on Signal received event. Many files changed because of End Of Line switch to Unix style.

--- ce_main_app/config/netsettings.h
+++ ce_main_app/config/netsettings.h
@@ -1,1 +1,49 @@
-#ifndef _NETSETTINGS_H_#define _NETSETTINGS_H_#include <string>typedef struct {	bool		dhcpNotStatic;	std::string address;    std::string netmask;    std::string gateway;		std::string wpaSsid;	std::string wpaPsk;} TNetInterface;class NetworkSettings {public:	NetworkSettings(void);	void load(void);	void save(void);    void updateResolvConf(void);		TNetInterface	eth0;	TNetInterface	wlan0;	std::string		nameserver;    std::string     hostname;private:	void initNetSettings(TNetInterface *neti);	void readString(char *line, char *tag, std::string &val, bool singleWordLine);	void dumpSettings(void);		void loadNameserver(void);	void saveNameserver(void);        void loadWpaSupplicant(void);    void saveWpaSupplicant(void);        void writeNetInterfaceSettings(FILE *f, TNetInterface &iface, char *ifaceName);        void replaceIPonDhcpIface(void);};#endif
+#ifndef _NETSETTINGS_H_
+#define _NETSETTINGS_H_
+
+#include <string>
+
+typedef struct {
+	bool		dhcpNotStatic;
+	std::string address;
+    std::string netmask;
+    std::string gateway;
+	
+	std::string wpaSsid;
+	std::string wpaPsk;
+} TNetInterface;
+
+class NetworkSettings 
+{
+public:
+	NetworkSettings(void);
+
+	void load(void);
+	void save(void);
+
+    void updateResolvConf(void);
+	
+	TNetInterface	eth0;
+	TNetInterface	wlan0;
+	std::string		nameserver;
+    std::string     hostname;
+
+private:
+
+	void initNetSettings(TNetInterface *neti);
+	void readString(char *line, char *tag, std::string &val, bool singleWordLine);
+	void dumpSettings(void);
+	
+	void loadNameserver(void);
+	void saveNameserver(void);
+    
+    void loadWpaSupplicant(void);
+    void saveWpaSupplicant(void);
+    
+    void writeNetInterfaceSettings(FILE *f, TNetInterface &iface, char *ifaceName);
+    
+    void replaceIPonDhcpIface(void);
+};
+
+#endif
+
