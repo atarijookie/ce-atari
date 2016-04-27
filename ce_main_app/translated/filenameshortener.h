c470ebffe67e04fbd2ef603f12fa56e17a3e8485
Thomas Bernard <miniupnp@free.fr>
2016-04-27 17:42:16 +0200

    filenameshortener: char * => const char * when needed

--- ce_main_app/translated/filenameshortener.h
+++ ce_main_app/translated/filenameshortener.h
@@ -39,8 +39,8 @@ private:
 
     std::map<std::string, std::string> mapFilenameNoExt;                    // used by shortenName() to create unique file name with ~
 
-    bool shortenName(char *nLong, char *nShort);
-    bool shortenExtension(char *shortFileName, char *nLongExt, char *nShortExt);
+    bool shortenName(const char *nLong, char *nShort);
+    bool shortenExtension(const char *shortFileName, const char *nLongExt, char *nShortExt);
 
     static int  strCharPos(const char *str, int maxLen, char ch);
     static void replaceNonLetters(char *str);
