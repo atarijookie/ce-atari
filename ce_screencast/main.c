10b85553f38a1c30f057a13b80ae569e288e8a1c
tin <tin@absencehq.de>
2016-05-08 20:04:31 +0200

    -screencast: removed unnecessary IRQ masking -screencast: added resolution warning (only ST compatible resolutions are supported for now) -screencast: added SCSI support

--- ce_screencast/main.c
+++ ce_screencast/main.c
@@ -23,7 +23,7 @@ void showConnectionErrorMessage(void);
 BYTE findDevice(void);
 void getDriveConfig(void);
 extern void getConfig(void); 
-BYTE checkrez(void);
+BYTE checkForSupportedResolution(void);
 
 BYTE atariKeysToSingleByte(BYTE vkey, BYTE key);
 BYTE ce_identify(BYTE ACSI_id);
@@ -33,9 +33,9 @@ BYTE      deviceID;
 BYTE commandShort[CMD_LENGTH_SHORT]	= {			0, 'C', 'E', HOSTMOD_TRANSLATED_DISK, 0, 0};
 BYTE commandLong[CMD_LENGTH_LONG]	= {0x1f,	0, 'C', 'E', HOSTMOD_TRANSLATED_DISK, 0, 0, 0, 0, 0, 0, 0, 0}; 
 
-#define DMA_BUFFER_SIZE		512
+#define DMA_BUFFER_SIZE		512*4
 
-BYTE dmaBuffer[DMA_BUFFER_SIZE + 2];
+BYTE dmaBuffer[DMA_BUFFER_SIZE+2];
 BYTE *pDmaBuffer; 
 
 BYTE prevCommandFailed;
@@ -63,18 +63,21 @@ extern WORD _vblskipconfig;
 
 extern BYTE machine;
 
-char *version = "2016-05-02"; 
+char *version = "2016-05-08"; 
 
 volatile mutex mtx;   
 /*--------------------------------------------------*/
 int main(void)
 {
-	
+	BYTE found; 	
 	DWORD scancode;
 	BYTE key, vkey, res;
 	DWORD toEven;
 	void *OldSP;
 
+	//initialize lock
+	mutex_unlock(&mtx);
+ 
 	OldSP = (void *) Super((void *)0);  			/* supervisor mode */ 
 	
 	prevCommandFailed = 0;
@@ -95,15 +98,15 @@ int main(void)
     (void) Cconws(" ]\33q\r\n"); 		
 	/* ---------------------- */
 	/* search for device on the ACSI bus */
-	deviceID=findDevice();
-	if( deviceID == FALSE && 0==1 ){
+	found=findDevice();
+	if( !found ){
     	(void) Cconws("Quit."); 		
 	    Super((void *)OldSP);  			      /* user mode */
 		return 0;
 	}
 	/* ----------------- */
 
-	if( !checkrez() )
+	if( checkForSupportedResolution()==FALSE )
 	{
 		(void) Cconws("\r\n");
 		(void) Cconws("Wrong resolution!\r\n");
@@ -115,14 +118,21 @@ int main(void)
 		(void) Cconws("640*200*2\r\n");
 		(void) Cconws("\r\n");
 		(void) Cconws("Screencast not installed.\r\n");
-    	(void) Cconws("Press any key to quit, T to try anyway.\r\n"); 		
+    	(void) Cconws("Press any key to quit T to try anyway.\r\n"); 		
 		key=Cnecin();
 		if( key!='t' ) 
 		{
 	    	Super((void *)OldSP);  			      /* user mode */
 			return 0;
+		}else{
+			(void) Cconws("\r\n");
+			(void) Cconws("\33pWARNING\33q: this will probably crash.\r\n");
 		}
 	}
+
+	(void) Cconws("\r\n");
+	(void) Cconws("Do not change resolution after this point!\r\n");
+	(void) Cconws("\r\n");
 
 	/* now set up the acsi command bytes so we don't have to deal with this one anymore */
 	commandShort[0] = (deviceID << 5); 					/* cmd[0] = ACSI_id + TEST UNIT READY (0)	*/
@@ -174,7 +184,12 @@ void logMsgProgress(DWORD current, DWORD total)
 //    (void) Cconws("\n\r");
 }  
 
-BYTE checkrez(void)
+/*
+	Check if we have a resolution, that resembles an ST compatible screenbuffer
+	Problem: XBIOS lies on newer systems and with GFX cards, so try to determine
+	an ST compatible mode by combination of screen buffer size (F030) and screen dimension/colors
+*/
+BYTE checkForSupportedResolution(void)
 {
  	short vdi_work_in[11] = {1,1,1,1,1,1,1,1,1,1,2};
 	short vdi_work_out[57];
@@ -185,12 +200,9 @@ BYTE checkrez(void)
    	v_opnvwk(vdi_work_in, &vdi_handle, vdi_work_out);
 	v_clsvwk(vdi_handle);
 
-    scr_x=vdi_work_out[0]+12;
+    scr_x=vdi_work_out[0]+1;
 	scr_y=vdi_work_out[1]+1;
 	colors=vdi_work_out[13];
-//	colorsall=vdi_work_out[39];
-
-//>	printf("detected %d*%d*%d\r\n",scr_x,scr_y,colors);
 
 	switch(machine)
 	{
--- ce_screencast/screenirq.s
+++ ce_screencast/screenirq.s
@@ -14,32 +14,8 @@ _v_bas_ad   = 0x44e |.l
 _init_screencapture:
     movem.l D0-A6,-(SP)
 
-|   find empty slot in vblqueue
-|   move.w  _nvbls,D0
-|   move.l  _vblqueue,A0
-|   subq    #1,D0
-|vblfindloop:
-|   tst.l   (A0)
-|   beq.s   vblinstall
-|   addq.l  #4,A0
-|   dbra    D0,vblfindloop
-    
     move.l  0x70,__savevbl
     move.l  #screenirq,0x70                     | install VBL   
-
-|   clr.w   __installed
-|   lea     p1,A0
-|   jsr     cconws
-|   lea     p2,A0
-|   jsr     cconws
-|   lea     p3,A0
-|   jsr     cconws
-|   movem.l (SP)+,D0-A6
-|   rts
-
-|there is an empty vbl slot, so install worker in there
-|vblinstall:
-|   move.l  #screenirq,(A0)
 
     st      __installed
 
@@ -76,13 +52,10 @@ castdo:
 
     move.w  _flock,-(SP)                        | save flock
 
-    move.w  sr,-(SP)
-    move    #0x2700,sr                          | protect this from interrupts, making it almost atomic  
     tst.w   _flock                              | unfortunately we can't use tas, as the _flock word is perused by different drivers differently - TOS sets 1.w, other only st the first byte etc.
     bne.s   castskipflock                       | don't do anything if flock is set - we can't use the DMA then, anyways
     
     move.w  #-1,_flock                          | keep others from using DMA  
-    move.w  (SP)+,sr
 
     clr.w   (A1)                                | reset skip-counter for next frameskip
     jsr     (A0)                                | call worker
@@ -92,7 +65,6 @@ castskipvbl:
     rts
 
 castskipflock:
-    move.w  (SP)+,sr
     move.w  (SP)+,_flock                        | set flock to previous state
     rts
     
