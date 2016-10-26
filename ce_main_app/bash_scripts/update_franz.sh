306ce251adb02fda80e5ff563bb2fbe6e8fd5977
Miro Nohaj (Jookie) <joo@kie.sk>
2016-10-26 14:11:29 +0200

    Usage of the right serial port made - based on distro and existance of /dev/serial0 device.

--- ce_main_app/bash_scripts/update_franz.sh
+++ ce_main_app/bash_scripts/update_franz.sh
@@ -14,7 +14,18 @@ if [ ! -f /tmp/franz.hex ]; then            # if this file doesn't exist, try to
     fi
 fi
 
-/ce/update/flash_stm32 -y -w /tmp/franz.hex /dev/ttyAMA0
+#----------------------------------------
+# if symlink serial0 exists, use it, otherwise try to use ttyAMA0
+if [ -f /dev/serial0 ]; then
+    serialport="/dev/serial0"
+else
+    serialport="/dev/ttyAMA0"
+fi
+
+echo "Will use serial port: " $serialport
+#----------------------------------------
+
+/ce/update/flash_stm32 -y -w /tmp/franz.hex $serialport
 rm -f /tmp/franz.hex
 
 echo " "
--- ce_main_app/bash_scripts/update_hans.sh
+++ ce_main_app/bash_scripts/update_hans.sh
@@ -14,7 +14,18 @@ if [ ! -f /tmp/hans.hex ]; then                 # if this file doesn't exist, tr
     fi
 fi
 
-/ce/update/flash_stm32 -x -w /tmp/hans.hex /dev/ttyAMA0
+#----------------------------------------
+# if symlink serial0 exists, use it, otherwise try to use ttyAMA0
+if [ -f /dev/serial0 ]; then
+    serialport="/dev/serial0"
+else
+    serialport="/dev/ttyAMA0"
+fi
+
+echo "Will use serial port: " $serialport
+#----------------------------------------
+
+/ce/update/flash_stm32 -x -w /tmp/hans.hex $serialport
 rm -f /tmp/hans.hex
 
 echo " "
