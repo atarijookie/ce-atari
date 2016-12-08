atarijookie/ce-atari

commit fe2e7a8576774e3951088e365a471fa173812022

fix Scsi::detachMediaFromACSIidByIndex() clean media fields
fix an issue when trying to reattach a detached media

ce_main_app/native/scsi.cpp
@@ -249,14 +249,13 @@ void Scsi::detachMediaFromACSIidByIndex(int index)
     }
 
     int attMediaInd = devInfo[index].attachedMediaIndex;
-    attachedMedia[ attMediaInd ].devInfoIndex = -1;                   // set not attached in attached media
-
     if(    attachedMedia[attMediaInd].dataMediaDynamicallyAllocated) { // if dataMedia was creates using new, use delete
         if(attachedMedia[ attMediaInd ].dataMedia != NULL) {
             delete attachedMedia[ attMediaInd ].dataMedia;            // delete the data source access object
             attachedMedia[ attMediaInd ].dataMedia = NULL;
         }
     }
+    initializeAttachedMediaVars(attMediaInd);
 
     devInfo[index].attachedMediaIndex   = -1;                       // set not attached in dev info
     devInfo[index].accessType           = SCSI_ACCESSTYPE_NO_DATA;

commit 228f0f7f965a80bd29808ac27ac70015f4350be3

fix reload of native/scsi settings.
Problem was introduced by fe2e7a8576774e3951088e365a471fa173812022
I still need a way to remount RAW USB Sticks

ce_main_app/ccorethread.cpp
@@ -124,7 +124,6 @@ void CCoreThread::sharedObjects_create(ConfigService* configService, FloppyServi
 
     shared.scsi        = new Scsi();
     shared.scsi->setAcsiDataTrans(dataTrans);
-    shared.scsi->attachToHostPath(TRANSLATEDBOOTMEDIA_FAKEPATH, SOURCETYPE_IMAGE_TRANSLATEDBOOT, SCSI_ACCESSTYPE_FULL);
 
     shared.translated = new TranslatedDisk(dataTrans, configService, screencastService);
     shared.translated->setSettingsReloadProxy(&settingsReloadProxy);

ce_main_app/native/scsi.cpp
@@ -387,12 +387,18 @@ void Scsi::loadSettings(void)
         attachToHostPath(empty, SOURCETYPE_SD_CARD, SCSI_ACCESSTYPE_FULL);
     }
 
+    // attach Translated bootmedia
+    attachToHostPath(TRANSLATEDBOOTMEDIA_FAKEPATH, SOURCETYPE_IMAGE_TRANSLATEDBOOT, SCSI_ACCESSTYPE_FULL);
+
+    // TODO : attach RAW usb drives again
+#if 0
     // and now reattach everything back according to new ACSI ID settings
     for(int i=0; i<MAX_ATTACHED_MEDIA; i++) {
         if(attachedMedia[i].dataMedia != NULL) {            // if there's some media to use
             attachMediaToACSIid(i, attachedMedia[i].hostSourceType, attachedMedia[i].accessType);
         }
     }
+#endif
 
     std::string img;
     img = s.getString("HDDIMAGE", "");