9d526f82a779d0c067ebd4aec68f113ae2967063
Jookie (Miroslav Nohaj) <joo@kie.sk>
2017-01-03 03:46:54 +0100

    Setting fixed core_freq added in copynewscripts.sh to fix SPI issues on RPi 3.

--- ce_main_app/shellscripts/copynewscripts.sh
+++ ce_main_app/shellscripts/copynewscripts.sh
@@ -42,6 +42,7 @@ compareAndCopy   "/ce/update/update_app.sh"      "/ce/app/shellscripts/update_ap
 compareAndCopy   "/ce/update/update_franz.sh"    "/ce/app/shellscripts/update_franz.sh"
 compareAndCopy   "/ce/update/update_hans.sh"     "/ce/app/shellscripts/update_hans.sh"
 compareAndCopy   "/ce/update/update_xilinx.sh"   "/ce/app/shellscripts/update_xilinx.sh"
+compareAndCopy   "/ce/install_ffmpeg.sh"         "/ce/app/shellscripts/install_ffmpeg.sh"
 
 #---------------------------------------------------------------------------
 # then copy the scripts which are different for Raspbian and Yocto
@@ -50,12 +51,49 @@ issue=$( cat /etc/issue | grep -o "Yocto" | wc -l )
 # If at least once the Yocto was found, it's Yocto
 if [ "$issue" -gt "0" ]; then
     # for yocto
-    compareAndCopy   "/ce/wifisuper.sh"          "/ce/app/shellscripts/wifisuper.sh"         # update wifisuper.sh
+    compareAndCopy   "/ce/wifisuper.sh"          "/ce/app/shellscripts/wifisuper.sh"        # update wifisuper.sh
 else
     # for raspbian
-	rm -f /ce/wifisuper.sh                                                              # remove wifisuper.sh
+    rm -f /ce/wifisuper.sh                                                                  # remove wifisuper.sh
     compareAndCopy   "/ce/ceboot.sh"             "/ce/app/shellscripts/ceboot.sh"
-    compareAndCopy   "/etc/init.d/cosmosex"      "/ce/app/shellscripts/initd_cosmosex"       # update init.d script
+    compareAndCopy   "/etc/init.d/cosmosex"      "/ce/app/shellscripts/initd_cosmosex"      # update init.d script
+
+    #------------------------
+    # now add / change the core_freq param in the boot config to avoid SPI clock issues
+    coreFreqCountAny=$( cat /boot/config.txt | grep core_freq | wc -l )
+    coreFreqCountCorrect=$( cat /boot/config.txt | grep 'core_freq=250' | wc -l )
+
+    addCoreFreq=0           # don't add it yet
+
+    # A) no core_freq? Add it
+    if [ "$coreFreqCountAny" -eq "0" ]; then
+        echo "No core_freq in /boot/config.txt, adding it"
+        addCoreFreq=1
+    fi
+
+    # B) more than one core_freq? Remove them, then add one
+    if [ "$coreFreqCountAny" -gt "1" ]; then
+        echo "Too many core_freq in /boot/config.txt, fixing it"
+        addCoreFreq=1
+    fi
+
+    # C) There is some core_freq, but it's not correct? Remove it, then add correct one
+    if [ "$coreFreqCountAny" -gt "0" ] && [ "$coreFreqCountCorrect" -eq "0" ]; then
+        echo "core_freq in /boot/config.txt is incorrect, fixing it"
+        addCoreFreq=1
+    fi
+
+    # if we need to add the core_freq
+    if [ "$addCoreFreq" -gt "0" ]; then
+        mv /boot/config.txt /boot/config.old                            # back up old
+        cat /boot/config.old | grep -v 'core_freq' > /boot/config.txt   # create new without any core_freq directive
+
+        # now append the correct core_config on the end of file
+        echo "core_freq=250" >> /boot/config.txt
+    else
+        # we don't need to do anything for case D), where there is one core_freq there and it's correct
+        echo "core_freq is ok in /boot/config.txt"
+    fi
 fi
 
 echo "Doing sync..."
