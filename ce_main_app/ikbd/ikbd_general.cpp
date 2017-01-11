38901500b5d56103f2c2583fb8668945f1224cd8
Thomas Bernard <miniupnp@free.fr>
2017-01-11 10:31:06 +0100

    ikbd_general.cpp: fix whitespaces

--- ce_main_app/ikbd/ikbd_general.cpp
+++ ce_main_app/ikbd/ikbd_general.cpp
@@ -1,3 +1,4 @@
+// vim: shiftwidth=4 softtabstop=4 tabstop=4 expandtab
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,7 +7,7 @@
 #include <unistd.h>
 
 #include <signal.h>
-#include <termios.h> 
+#include <termios.h>
 #include <errno.h>
 
 #include <stdarg.h>
@@ -29,24 +30,24 @@ extern TFlags flags;                                // global flags from command
 
 void *ikbdThreadCode(void *ptr)
 {
-    struct termios	termiosStruct;
+    struct termios    termiosStruct;
     Ikbd ikbd;
     DWORD nextDevFindTime;
 
     ikbdLog("----------------------------------------------------------");
     ikbdLog("ikbdThreadCode will enter loop...");
-    
-    bcm2835_gpio_write(PIN_TX_SEL1N2, HIGH);		// TX_SEL1N2, switch the RX line to receive from Franz, which does the 9600 to 7812 baud translation
+
+    bcm2835_gpio_write(PIN_TX_SEL1N2, HIGH);        // TX_SEL1N2, switch the RX line to receive from Franz, which does the 9600 to 7812 baud translation
 
     nextDevFindTime = Utils::getEndTime(3000);      // check the devices in 3 seconds
     ikbd.findDevices();
     ikbd.findVirtualDevices();
-    
-	// open and set up uart
-	int res = ikbd.serialSetup(&termiosStruct);
-	if(res == -1) {
+
+    // open and set up uart
+    int res = ikbd.serialSetup(&termiosStruct);
+    if(res == -1) {
         logDebugAndIkbd(LOG_ERROR, "ikbd.serialSetup failed, won't be able to send IKDB data");
-	}
+    }
 
     while(sigintReceived == 0) {
         // reload config if needed
@@ -80,12 +81,12 @@ void *ikbdThreadCode(void *ptr)
             }
 
             if(i < 2) {     // for keyboard and mouse
-                res = read(ikbd.getFdByIndex(i), &ev, sizeof(input_event)); 
+                res = read(ikbd.getFdByIndex(i), &ev, sizeof(input_event));
             } else if (i<4) {        // for joysticks
-                res = read(ikbd.getFdByIndex(i), &js, sizeof(js_event)); 
+                res = read(ikbd.getFdByIndex(i), &js, sizeof(js_event));
             }
             if( i==4 || i==5) {     // for virtual mouse and keyboard
-                res = read(ikbd.getFdByIndex(i), &ev, sizeof(input_event)); 
+                res = read(ikbd.getFdByIndex(i), &ev, sizeof(input_event));
             }
             if(res < 0) {                                           // on error, skip the rest
                 if(errno == ENODEV) {                               // if device was removed, deinit it
@@ -115,14 +116,14 @@ void *ikbdThreadCode(void *ptr)
             }
         }
 
-        if(!gotSomething) {                                         // didn't succeed with reading of any device? 
+        if(!gotSomething) {                                         // didn't succeed with reading of any device?
             usleep(10000);                                          // sleep for 10 ms
             continue;
         }
     }
-    
+
     ikbd.closeDevs();
-    
+
     ikbdLog("ikbdThreadCode has quit");
     return 0;
 }
@@ -132,12 +133,12 @@ Ikbd::Ikbd()
     loadSettings();
 
     initDevs();
-    
+
     keyJoyKeys.setKeyTranslator(&keyTranslator);        // first set the translator
     keyJoyKeys.loadKeys();                              // load the keys used for keyb joys
-    
+
     ceIkbdMode = CE_IKBDMODE_SOLO;
-	
+
     fdUart      = -1;
     mouseBtnNow = 0;
 
@@ -166,14 +167,14 @@ void Ikbd::loadSettings(void)
         joy1st = INTYPE_JOYSTICK2;
         joy2nd = INTYPE_JOYSTICK1;
     }
-    
+
     // get enabled flags for mouse wheel as keys
     mouseWheelAsArrowsUpDown = s.getBool((char *) "MOUSE_WHEEL_AS_KEYS", true);
-    
+
     // get enabled flags for keyb joys
     keybJoy0 = s.getBool("KEYBORD_JOY0", false);
     keybJoy1 = s.getBool("KEYBORD_JOY1", false);
-    
+
     keyJoyKeys.setKeyTranslator(&keyTranslator);        // first set the translator
     keyJoyKeys.loadKeys();                              // load the keys used for keyb joys
 }
@@ -187,77 +188,77 @@ void Ikbd::resetInternalIkbdVars(void)
     mouseY0atTop    = true;
     mouseAbsBtnAct  = MOUSEBTN_REPORT_NOTHING;
 
-    absMouse.maxX   	= 640;
-    absMouse.maxY   	= 400;
-    absMouse.x      	= 0;
-    absMouse.y      	= 0;
-	absMouse.buttons	= 0;
-	absMouse.scaleX		= 1;
-	absMouse.scaleY		= 1;
+    absMouse.maxX       = 640;
+    absMouse.maxY       = 400;
+    absMouse.x          = 0;
+    absMouse.y          = 0;
+    absMouse.buttons    = 0;
+    absMouse.scaleX        = 1;
+    absMouse.scaleY        = 1;
 
-	relMouse.threshX	= 1;
-	relMouse.threshY	= 1;
-	
-	keycodeMouse.deltaX	= 0;
-	keycodeMouse.deltaY	= 0;
-	
+    relMouse.threshX    = 1;
+    relMouse.threshY    = 1;
+
+    keycodeMouse.deltaX    = 0;
+    keycodeMouse.deltaY    = 0;
+
     joystickMode    = JOYMODE_EVENT;
     joystickEnabled = true;
 }
 
-int Ikbd::serialSetup(termios *ts) 
+int Ikbd::serialSetup(termios *ts)
 {
-	int fd;
-	
+    int fd;
+
     fdUart = -1;
 
-	fd = open(UARTFILE, O_RDWR | O_NOCTTY | O_NDELAY | O_NONBLOCK);
-	if(fd == -1) {
+    fd = open(UARTFILE, O_RDWR | O_NOCTTY | O_NDELAY | O_NONBLOCK);
+    if(fd == -1) {
         logDebugAndIkbd(LOG_ERROR, "Failed to open %s", UARTFILE);
         return -1;
-	}
-    
-	fcntl(fd, F_SETFL, 0);
-	tcgetattr(fd, ts);
+    }
 
-	/* reset the settings */
-	cfmakeraw(ts);
-	ts->c_cflag &= ~(CSIZE | CRTSCTS);
-	ts->c_iflag &= ~(IXON | IXOFF | IXANY | IGNPAR);
-	ts->c_lflag &= ~(ECHOK | ECHOCTL | ECHOKE);
-	ts->c_oflag &= ~(OPOST | ONLCR);
+    fcntl(fd, F_SETFL, 0);
+    tcgetattr(fd, ts);
 
-	/* setup the new settings */
-	cfsetispeed(ts, B19200);
-	cfsetospeed(ts, B19200);
-	ts->c_cflag |=  CS8 | CLOCAL | CREAD;			// uart: 8N1
+    /* reset the settings */
+    cfmakeraw(ts);
+    ts->c_cflag &= ~(CSIZE | CRTSCTS);
+    ts->c_iflag &= ~(IXON | IXOFF | IXANY | IGNPAR);
+    ts->c_lflag &= ~(ECHOK | ECHOCTL | ECHOKE);
+    ts->c_oflag &= ~(OPOST | ONLCR);
 
-	ts->c_cc[VMIN ] = 0;
-	ts->c_cc[VTIME] = 0;
+    /* setup the new settings */
+    cfsetispeed(ts, B19200);
+    cfsetospeed(ts, B19200);
+    ts->c_cflag |=  CS8 | CLOCAL | CREAD;            // uart: 8N1
 
-	/* set the settings */
-	tcflush(fd, TCIFLUSH); 
-	
-	if (tcsetattr(fd, TCSANOW, ts) != 0) {
-		close(fd);
-		return -1;
-	}
+    ts->c_cc[VMIN ] = 0;
+    ts->c_cc[VTIME] = 0;
 
-	/* confirm they were set */
-	struct termios settings;
-	tcgetattr(fd, &settings);
-	if (settings.c_iflag != ts->c_iflag ||
-		settings.c_oflag != ts->c_oflag ||
-		settings.c_cflag != ts->c_cflag ||
-		settings.c_lflag != ts->c_lflag) {
-		close(fd);
-		return -1;
-	}
+    /* set the settings */
+    tcflush(fd, TCIFLUSH);
+
+    if (tcsetattr(fd, TCSANOW, ts) != 0) {
+        close(fd);
+        return -1;
+    }
+
+    /* confirm they were set */
+    struct termios settings;
+    tcgetattr(fd, &settings);
+    if (settings.c_iflag != ts->c_iflag ||
+        settings.c_oflag != ts->c_oflag ||
+        settings.c_cflag != ts->c_cflag ||
+        settings.c_lflag != ts->c_lflag) {
+        close(fd);
+        return -1;
+    }
 
     fcntl(fd, F_SETFL, FNDELAY);                    // make reading non-blocking
 
     fdUart = fd;
-	return fd;
+    return fd;
 }
 
 int Ikbd::fdWrite(int fd, BYTE *bfr, int cnt)
@@ -266,11 +267,11 @@ int Ikbd::fdWrite(int fd, BYTE *bfr, int cnt)
     if(flags.ikbdLogs) {
         std::string text = "sending to ST: ";
         char tmp[16];
-        
+
         for(int i=0; i<cnt; i++) {
             sprintf(tmp, "%02x ", bfr[i]);
             text += tmp;
-        }    
+        }
         ikbdLog(text.c_str());
     }
 #endif
@@ -292,34 +293,33 @@ void ikbdLog(const char *format, ...)
     if(!flags.ikbdLogs) {               // don't do IKBD logs? quit
         return;
     }
-    
+
     static DWORD prevLogOutIkbd = 0;
 
     va_list args;
     va_start(args, format);
 
-	FILE *f;
+    FILE *f;
 
     f = fopen("/var/log/ikbdlog.txt", "a+t");
-	
-	if(!f) {
-		printf("%08d: ", Utils::getCurrentMs());
-		vprintf(format, args);
-		printf("\n");
+
+    if(!f) {
+        printf("%08d: ", Utils::getCurrentMs());
+        vprintf(format, args);
+        printf("\n");
 
         va_end(args);
         return;
-	}
+    }
 
-	DWORD now = Utils::getCurrentMs();
-	DWORD diff = now - prevLogOutIkbd;
-	prevLogOutIkbd = now;
-	
-	fprintf(f, "%08d\t%08d\t ", now, diff);
+    DWORD now = Utils::getCurrentMs();
+    DWORD diff = now - prevLogOutIkbd;
+    prevLogOutIkbd = now;
+
+    fprintf(f, "%08d\t%08d\t ", now, diff);
     vfprintf(f, format, args);
-	fprintf(f, "\n");
-	fclose(f);
+    fprintf(f, "\n");
+    fclose(f);
 
     va_end(args);
 }
-
