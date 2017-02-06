139feb8a4a7424630c86937bac9b5cb63bbf98c5
atarijookie <joo@kie.sk>
2017-02-06 13:45:51 +0100

    Added simple test for Exxos

---
+++ test_exxos/gemdos.h
@@ -0,0 +1,175 @@
+#ifndef GEMDOS_H
+#define GEMDOS_H
+
+// path functions
+#define GEMDOS_Dsetdrv      0x0e
+#define GEMDOS_Dgetdrv      0x19
+#define GEMDOS_Dsetpath     0x3b
+#define GEMDOS_Dgetpath     0x47
+
+// directory & file search
+#define GEMDOS_Fsetdta      0x1a
+#define GEMDOS_Fgetdta      0x2f
+#define GEMDOS_Fsfirst      0x4e
+#define GEMDOS_Fsnext       0x4f
+
+// file and directory manipulation
+#define GEMDOS_Dfree        0x36
+#define GEMDOS_Dcreate      0x39
+#define GEMDOS_Ddelete      0x3a
+#define GEMDOS_Frename      0x56
+#define GEMDOS_Fdatime      0x57
+#define GEMDOS_Fdelete      0x41
+#define GEMDOS_Fattrib      0x43
+
+// file content functions
+#define GEMDOS_Fcreate      0x3c
+#define GEMDOS_Fopen        0x3d
+#define GEMDOS_Fclose       0x3e
+#define GEMDOS_Fread        0x3f
+#define GEMDOS_Fwrite       0x40
+#define GEMDOS_Fseek        0x42
+
+// date and time function
+#define GEMDOS_Tgetdate     0x2A
+#define GEMDOS_Tsetdate     0x2B
+#define GEMDOS_Tgettime     0x2C
+#define GEMDOS_Tsettime     0x2D
+
+#define GD_CUSTOM_initialize    0x60
+#define GD_CUSTOM_getConfig     0x61
+#define GD_CUSTOM_ftell         0x62
+#define GD_CUSTOM_getRWdataCnt  0x63
+#define GD_CUSTOM_Fsnext_last   0x64
+#define GD_CUSTOM_getBytesToEOF 0x65
+
+// BIOS functions we need to support
+#define BIOS_Drvmap				0x70
+#define BIOS_Mediach			0x71
+#define BIOS_Getbpb				0x72
+
+// other functions
+#define ACC_GET_MOUNTS			0x80
+#define ACC_UNMOUNT_DRIVE       0x81
+
+#define ST_LOG_TEXT             0x85
+
+#define TEST_READ               0x90
+#define TEST_WRITE              0x91
+#define TEST_GET_ACSI_IDS       0x92
+
+//////////////////////////////////////
+/*
+
+Functions which require more than SCSI(6) length so they wouldn't have to be done in 2 acsi_cmd calls:
+GEMDOS_Fseek:  5 + 6
+GEMDOS_Fread:  6 + 4
+GEMDOS_Fwrite: 6 + 4
+
+Functions which can be implemented with SCSI(6) but would be nicer to do them with something more:
+GEMDOS_Fdatime: 6 + 4    -- would be ACSI_READ only then
+
+*/
+//////////////////////////////////////
+
+#define MAX_FILES       	40          // maximum open files count, 40 is the value from EmuTOS
+
+#define DTA_BUFFER_SIZE		512			// size of buffer we use to store buffered DTAs on Atari
+
+// file attributes
+/*
+#define FA_READONLY     (1 << 0)
+#define FA_HIDDEN       (1 << 1)
+#define FA_SYSTEM       (1 << 2)
+#define FA_VOLUME       (1 << 3)
+#define FA_DIR          (1 << 4)
+#define FA_ARCHIVE      (1 << 5)
+*/
+
+
+// struct used for Fsfirst and Fsnext - modified version without first 21 reserved bytes
+// now the struct has 23 bytes total, so a buffer of 512 bytes should contain 22 of these + 6 spare bytes
+typedef struct
+{
+    BYTE    d_attrib;       // GEMDOS File Attributes
+    WORD    d_time;         // GEMDOS Time
+    WORD    d_date;         // GEMDOS Date
+    DWORD   d_length;       // File Length
+    char    d_fname[14];    // Filename
+} DTAshort;
+
+
+// list of standard handles used with fdup
+#define GSH_CONIN   0   // con: Standard input (defaults to whichever BIOS device is mapped to GEMDOS handle -1)
+#define GSH_CONOUT  1   // con: Standard output (defaults to whichever BIOS device is mapped to GEMDOS handle -1)
+#define GSH_AUX     2   // aux: Currently mapped serial device (defaults to whichever BIOS device is mapped to GEMDOS handle -2)
+#define GSH_PRN     3   // prn: Printer port (defaults to whichever BIOS device is currently mapped to GEMDOS handle -3).
+#define GSH_RES1    4   // None Reserved
+#define GSH_RES2    5   // None Reserved
+#define GSH_BIOSCON -1  // None Refers to BIOS handle 2. This handle may only be redirected under the presence of MiNT. Doing so redirects output of the BIOS.
+#define GSH_BIOSAUX -2  // None Refers to BIOS handle 1. This handle may only be redirected under the presence of MiNT. Doing so redirects output of the BIOS.
+#define GSH_BIOSPRN -3  // None Refers to BIOS handle 0. This handle may only be redirected under the presence of MiNT. Doing so redirects output of the BIOS.
+#define GSH_MIDIIN  -4
+#define GSH_MIDIOUT -5
+
+void initFunctionTable(void);
+
+WORD getDriveFromPath(char *path);
+BYTE isOurDrive(WORD drive, BYTE withCurrentDrive);
+void updateCeDrives(void);
+
+int32_t custom_fread ( void *sp );
+int32_t custom_fwrite( void *sp );
+int32_t custom_pexec( void *sp );
+int32_t custom_pterm( void *sp );
+int32_t custom_pterm0( void *sp );
+
+BYTE commitChanges(WORD ceHandle);
+void initFileBuffer(WORD ceHandle);
+
+BYTE fillReadBuffer(WORD ceHandle);
+DWORD readData(WORD ceHandle, BYTE *bfr, DWORD cnt, BYTE seekOffset);
+void seekInFileBuffer(WORD ceHandle, int32_t offset, BYTE seekMode);
+
+DWORD writeData(BYTE ceHandle, BYTE *bfr, DWORD cnt);
+
+#define CALL_OLD_GD( function, ... )	\
+		useOldGDHandler = 1;			\
+		res = function( __VA_ARGS__ );	\
+		return res;
+
+#define CALL_OLD_GD_NORET( function, ... )	\
+		useOldGDHandler = 1;			\
+		res = function( __VA_ARGS__ );
+
+#define CALL_OLD_GD_VOIDRET( function, ... )	\
+		useOldGDHandler = 1;			\
+		function( __VA_ARGS__ );
+		
+// if sign bit is set, extend the sign to whole DWORD
+#define extendByteToDword(X)    ( ((X & 0x80)==0) ? X : (0xffffff00 | X) )
+
+// macros for getting data from buffer - defined for big and little endian
+// big endian (atari)
+#define getWord(POINTER)        ((WORD)  *((WORD  *) POINTER))
+#define getDword(POINTER)       ((DWORD) *((DWORD *) POINTER))
+
+// little endian (pc)
+//#define getWord(POINTER)        ( (((WORD) *POINTER)<<8) | ((WORD) *(POINTER+1)))
+//#define getDword(POINTER)       ( (((DWORD) *POINTER)<<24) | (((DWORD) *(POINTER+1))<<16) | (((DWORD) *(POINTER+2))<<8) | (((DWORD) *(POINTER+3))) )
+
+
+		
+// The following macros are used to convert atari handle numbers which are WORDs
+// to CosmosEx ex handle numbers, which are only BYTEs; and back.
+// To mark the difference between normal Atari handle and handle which came 
+// from CosmosEx I've added some offset to CosmosEx handles.
+
+// CosmosEx file handle:              0 ...  40
+// Atari handle for regular files:    0 ...  90
+// Atari handle for CosmosEx files: 150 ... 200
+#define handleIsFromCE(X)		(X >= 150 && X <= 200)
+#define handleAtariToCE(X)		(X  - 150)
+#define handleCEtoAtari(X)		(X  + 150)		
+		
+#endif // GEMDOS_H
---
+++ test_exxos/main.c
@@ -0,0 +1,242 @@
+//--------------------------------------------------
+#include <mint/osbind.h> 
+#include <mint/linea.h> 
+#include <stdio.h>
+
+#include "acsi.h"
+#include "gemdos.h"
+#include "gemdos_errno.h"
+#include "VT52.h"
+#include "version.h"
+#include "hdd_if.h"
+#include "stdlib.h"
+
+//--------------------------------------------------
+
+void sleep(int seconds);
+
+void showHexByte(BYTE val);
+void showHexDword(DWORD val);
+
+//--------------------------------------------------
+BYTE deviceID;
+
+BYTE readBuffer [2 * 512];
+BYTE writeBuffer[2 * 512];
+BYTE *rBuffer, *wBuffer;
+
+void hdIfCmdAsUser(BYTE readNotWrite, BYTE *cmd, BYTE cmdLength, BYTE *buffer, WORD sectorCount);
+int  getIntFromUser(void);
+
+//--------------------------------------------------
+int main(void)
+{
+    BYTE key;
+    DWORD toEven;
+
+    //----------------------
+    // read all the keys which are waiting, so we can ignore them
+    while(1) {
+        if(Cconis() != 0) {             // if some key is waiting, read it
+            Cnecin();
+        } else {                        // no key is waiting, quit the loop
+            break;
+        }
+    }
+    //----------------------
+    hdd_if_select(IF_ACSI);
+    hdIf.maxRetriesCount = 0;
+    
+    // ---------------------- 
+    // create buffer pointer to even address 
+    toEven = (DWORD) &readBuffer[0];
+  
+    if(toEven & 0x0001)       // not even number? 
+        toEven++;
+  
+    rBuffer = (BYTE *) toEven; 
+
+    //----------
+    toEven = (DWORD) &writeBuffer[0];
+  
+    if(toEven & 0x0001)       // not even number? 
+        toEven++;
+  
+    wBuffer = (BYTE *) toEven; 
+
+    Clear_home();
+    VT52_Wrap_on();
+
+    (void) Cconws("\33pDestructive continous R/W test.\33q\r\n");
+    (void) Cconws("This will corrupt data on drive\r\n");
+    (void) Cconws("above 100 MB. Press 'c' to continue.\r\n");
+    
+    while(1) {
+        key = Cnecin();
+    
+        if(key == 'c' || key == 'C') {
+            break;
+        }
+
+        if(key == 'q' || key == 'Q') {
+            return 0;
+        }
+    }
+    
+    //-------------
+
+    (void) Cconws("Enter ACSI ID of device: ");
+    BYTE acsiId = getIntFromUser();
+    (void) Cconws("\r\n");
+    
+    //-------------
+    // fill write buffer
+    int i;
+    for(i=0; i<512; i++) {
+        wBuffer[i] = i;
+    }
+    
+    DWORD sectorStart   = 0x032000;     // starting sector: at 100 MB
+    DWORD sectorEnd     = 0x1FFFFF;     // ending   sector: at   1 GB
+    
+    DWORD sector        = sectorStart;
+    BYTE cmd[6];
+    
+    while(1) {
+        if(sector >= sectorEnd) {       // if doing last sector
+            sector = sectorStart;       // go to starting sector
+        }
+        
+        cmd[0] = (acsiId << 5) | 0x08;  // read
+        
+        cmd[1] = sector >> 16;          // sector number
+        cmd[2] = sector >>  8;
+        cmd[3] = sector      ;
+        
+        cmd[4] = 1;                     // sector count
+        cmd[5] = 0;                     // control byte
+     
+        sector++;                       // next time use next sector
+     
+        wBuffer[0] = sector >> 16;      // store sector # at first bytes, so the data changes all the time
+        wBuffer[1] = sector >>  8;
+        wBuffer[2] = sector      ;
+        
+        hdIfCmdAsUser(ACSI_WRITE, cmd, CMD_LENGTH_SHORT, wBuffer, 1);
+    
+        if(!hdIf.success || hdIf.statusByte != 0) {     // write failed?
+            Cconout('W');
+            continue;
+        }
+    
+        hdIfCmdAsUser(ACSI_READ, cmd, CMD_LENGTH_SHORT, rBuffer, 1);
+
+        if(!hdIf.success || hdIf.statusByte != 0) {     // read failed?
+            Cconout('R');
+            continue;
+        }
+        
+        BYTE res = memcomp(wBuffer, rBuffer, 512);      // check data
+
+        if(res == 0) {                                  // data OK?
+            Cconout('*');
+        } else {                                        // data fail?
+            Cconout('x');
+        }       
+    }
+    
+    return 0;
+}
+
+BYTE showQuestionGetBool(const char *question, BYTE trueKey, const char *trueWord, BYTE falseKey, const char *falseWord)
+{
+    // show question
+    (void) Cconws(question);
+
+    while(1) {
+        BYTE key = Cnecin();
+
+        if(key >= 'A' && key <= 'Z') {          // upper case letter? to lower case
+            key += 32;
+        }
+
+        if(key == trueKey) {                    // yes
+            (void) Cconws(" ");
+            (void) Cconws(trueWord);
+            (void) Cconws("\r\n");
+            return 1;
+        }
+
+        if(key == falseKey) {                   // no
+            (void) Cconws(" ");
+            (void) Cconws(falseWord);
+            (void) Cconws("\r\n");
+            return 0;
+        }
+    }
+}
+
+int getIntFromUser(void)
+{
+    BYTE key;
+    
+    while(1) {
+        key = Cnecin();
+
+        if(key >= '0' && key <= '9') {
+            break;
+        }
+    }
+    
+    Cconout(key);
+    return (key - '0');
+}
+
+void showHexByte(BYTE val)
+{
+    int hi, lo;
+    char tmp[3];
+    char table[16] = {"0123456789ABCDEF"};
+    
+    hi = (val >> 4) & 0x0f;;
+    lo = (val     ) & 0x0f;
+
+    tmp[0] = table[hi];
+    tmp[1] = table[lo];
+    tmp[2] = 0;
+    
+    (void) Cconws(tmp);
+}
+
+void showHexDword(DWORD val)
+{
+    showHexByte((BYTE) (val >> 24));
+    showHexByte((BYTE) (val >> 16));
+    showHexByte((BYTE) (val >>  8));
+    showHexByte((BYTE)  val);
+}
+
+//--------------------------------------------------
+// global variables, later used for calling hdIfCmdAsSuper
+BYTE __readNotWrite, __cmdLength;
+WORD __sectorCount;
+BYTE *__cmd, *__buffer;
+
+void hdIfCmdAsSuper(void)
+{
+    // this should be called through Supexec()
+    (*hdIf.cmd)(__readNotWrite, __cmd, __cmdLength, __buffer, __sectorCount);
+}
+
+void hdIfCmdAsUser(BYTE readNotWrite, BYTE *cmd, BYTE cmdLength, BYTE *buffer, WORD sectorCount)
+{
+    // store params to global vars
+    __readNotWrite  = readNotWrite;
+    __cmd           = cmd;
+    __cmdLength     = cmdLength;
+    __buffer        = buffer;
+    __sectorCount   = sectorCount;    
+    
+    // call the function which does the real work, and uses those global vars
+    Supexec(hdIfCmdAsSuper);
+}
