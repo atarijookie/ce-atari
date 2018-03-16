c2ec7e25d13d768b53bdcacfed0e192c41f2aaf8
Thomas Bernard <miniupnp@free.fr>
2018-03-16 11:22:10 +0100

    .install-curl.sh: make -j3

--- .install-curl.sh
+++ .install-curl.sh
@@ -8,12 +8,15 @@ URL="https://curl.haxx.se/download/${CURLARCHIVE}"
 if [ -z "${HOST}" ] ; then
   HOST=arm-linux-gnueabihf
 fi
+if [ -z "${MAKE}" ] ; then
+  MAKE=make
+fi
 
 wget ${URL} || exit 1
 tar xjf ${CURLARCHIVE} || exit 1
 cd ${CURL} || exit 1
 CC="${HOST}-gcc" ./configure --prefix=/usr/${HOST} --host=${HOST} --disable-shared --disable-manual || exit 1
-make || exit 1
-sudo make install || exit 1
+${MAKE} -j3 || exit 1
+sudo ${MAKE} install || exit 1
 
 exit 0
