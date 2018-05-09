0e1ca71b6e4d9b84b49ab3cb21a854708c31817a
Miro Nohaj <nohaj@datacrea.sk>
2018-05-09 16:46:10 +0200

    Removed selecting image list, fixed input list retrieving.

--- ce_main_app/webserver/api/resource/downloadresource.cpp
+++ ce_main_app/webserver/api/resource/downloadresource.cpp
@@ -52,20 +52,9 @@ bool DownloadResource::dispatch(mg_connection *conn, mg_request_info *req_info, 
             return true;
         }
 
-        if(strcmp(path, "havelists") == 0) {		// download/havelists -- return list of lists of games we can search through
-            std::ostringstream stringStream;
-            stringStream << "{\"havelists\": [ {\"list_is_floppy_images\": 1, \"list_name_humanreadable\": \"games on exxos\", \"list_filename\": \"automation_exxos\"}, {\"list_is_floppy_images\": 1, \"list_name_humanreadable\": \"games on stonish\", \"list_filename\": \"stonish_games\"} ]}";
-
-            std::string sJson = stringStream.str();
-            mg_printf(conn, "Content-Length: %lu\r\n\r\n",(unsigned long) sJson.length());	// Always set Content-Length
-            mg_write(conn, sJson.c_str(), sJson.length());	// send content
-            return true;
-        }
-
         if(strcmp(path, "imagelist") == 0) {		// download/imagelist -- return list images which we can search trhough
             std::ostringstream stringStream;
-            stringStream << "{\"imagelist\": [{\"list_filename\":\"automation_exxos\", \"csv\": \"http://www.exxoshost.co.uk/atari/games/automation/files/A_000.zip,0x0000,Sinbad and the Throne of the Falcon,Myth\nhttp://www.exxoshost.co.uk/atari/games/automation/files/A_001.zip,0x0000,Crystal Castles,Star Wars: The Empire Strikes Back,Northstar w/ docs,Star Raiders,Robotron: 2084,Trail Blazer,Football Manager: World Cup Edition\nhttp://www.exxoshost.co.uk/atari/games/automation/files/A_002.zip,0x0000,Into the Eagle's Nest,Trantor: The Last Stormtrooper,Extensor,Knightmare,Plutos,Virus,Joe Blade,Trifide II\n\"},\
-                                              {\"list_filename\":\"stonish_games\",    \"csv\": \"http://www.stonish.net/download/ACO_A.ZIP,0x0000,Vroom,Grav,FastCopy Pro 1.0c,FastCopy Pro 1.0c (doc)\nhttp://www.stonish.net/download/ACO_B.ZIP,0x0000,Flight Of The Intruder,Ripper 3.1,Musix Ripper 2.0,Low Res Grapic Ripper\nhttp://www.stonish.net/download/ACE5.ZIP,0x0000,Boston Bomb Club,Head Over Heels,The Blues Brothers,Neochrome Master 2.25\n\"} ]}";
+            stringStream << "{\"imagelist\": \"http://www.exxoshost.co.uk/atari/games/automation/files/A_000.zip,0x0000,Sinbad and the Throne of the Falcon,Myth\\nhttp://www.exxoshost.co.uk/atari/games/automation/files/A_001.zip,0x0000,Crystal Castles,Star Wars: The Empire Strikes Back,Northstar w/ docs,Star Raiders,Robotron: 2084,Trail Blazer,Football Manager: World Cup Edition\\nhttp://www.exxoshost.co.uk/atari/games/automation/files/A_002.zip,0x0000,Into the Eagle's Nest,Trantor: The Last Stormtrooper,Extensor,Knightmare,Plutos,Virus,Joe Blade,Trifide II\\n\"}";
 
             std::string sJson = stringStream.str();
             mg_printf(conn, "Content-Length: %lu\r\n\r\n",(unsigned long) sJson.length());	// Always set Content-Length
