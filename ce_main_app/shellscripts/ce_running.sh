ca1ea220e6815525dd1cacf4c51781ffe4d60169
Jookie (Miroslav Nohaj) <joo@kie.sk>
2018-05-09 07:27:23 +0200

    Fx for ce_stop.sh not being able to terminate ce when runned through systemctl

--- ce_main_app/shellscripts/ce_running.sh
+++ ce_main_app/shellscripts/ce_running.sh
@@ -24,10 +24,31 @@ ce_is_running() {
 
 # check if ce is running
 ce_is_running
+ce_running=$?
+
+# check status through systemctl 
+sysctl=$( systemctl status cosmosex )
+sc_notfound=$( echo $sysctl | grep 'Loaded: not-found' | wc -l )
+sc_running=$( echo $sysctl | grep 'Active: active' | wc -l )
+sc_notrunning=$( echo $sysctl | grep 'Active: failed' | wc -l )
 
 # if CE not running, quit right away
-if [ $? -eq 0 ]; then
+if [ "$ce_running" -eq "0" ]; then
     echo "cosmosex NOT running"
 else
     echo "cosmosex IS running"
 fi
+
+# now show the systemd service status
+if [ "$sc_notfound" -eq "1" ]; then
+    echo "cosmosex not installed as systemd service"
+else
+    if [ "$sc_running" -eq "1" ]; then
+        echo "cosmosex IS running as systemd service"
+    fi
+
+    if [ "$sc_notrunning" -eq "1" ]; then
+        echo "cosmosex NOT running as systemd service"
+    fi
+fi
+
--- ce_main_app/shellscripts/ce_stop.sh
+++ ce_main_app/shellscripts/ce_stop.sh
@@ -30,37 +30,55 @@ killall -9 cesuper.sh > /dev/null 2> /dev/null
 ce_is_running
 
 # if CE not running, quit right away
-if [ $? -eq 0 ]; then
+if [ "$?" -eq "0" ]; then
     echo "Terminating cosmosex not needed, no instance is running"
     exit
 fi
 
+#------------------------
+# if should terminate (also) using systemctl, do it
+if [ "$1" != "nosystemctl" ]; then
+    # try to find out if cosmosex.service is installed
+    sysctl=$( systemctl status cosmosex )
+    sc_notfound=$( echo $sysctl | grep 'Loaded: not-found' | wc -l )
+
+    # if the cosmosex.service is installed (the not-found string was not found)
+    if [ "$sc_notfound" -eq "0" ]; then
+        # try to stop it using systemctl
+        systemctl stop cosmosex
+    fi
+fi
+
+#------------------------
+
 # send SIGINT to allow terminate the app nicely
 echo "Terminating cosmosex - SIGINT"
-killall -2 cosmosex          > /dev/null 2> /dev/null
-killall -2 cosmosex_raspbian > /dev/null 2> /dev/null
-killall -2 cosmosex_yocto    > /dev/null 2> /dev/null
+killall -2 cosmosex                  > /dev/null 2> /dev/null
+killall -2 cosmosex_raspbian         > /dev/null 2> /dev/null
+killall -2 cosmosex_raspbian_stretch > /dev/null 2> /dev/null
+killall -2 cosmosex_raspbian_jessie  > /dev/null 2> /dev/null
+killall -2 cosmosex_yocto            > /dev/null 2> /dev/null
 
 # wait a while
 echo "Waiting for cosmosex to terminate nicely..."
 
 # as Yocto and Raspbian shells behave differently, there could be a loop here which would work on both, but...
 ce_is_running
-if [ $? -eq 0 ]; then
+if [ "$?" -eq "0" ]; then
     echo "cosmosex terminated"
     exit
 fi
 sleep 1
 
 ce_is_running
-if [ $? -eq 0 ]; then
+if [ "$?" -eq "0" ]; then
     echo "cosmosex terminated"
     exit
 fi
 sleep 1
 
 ce_is_running
-if [ $? -eq 0 ]; then
+if [ "$?" -eq "0" ]; then
     echo "cosmosex terminated"
     exit
 fi
@@ -68,6 +86,8 @@ sleep 1
 
 # send SIGKILL to terminate the app if it didn't stop nicely
 echo "Terminating cosmosex - SIGKILL"
-killall -9 cosmosex          > /dev/null 2> /dev/null
-killall -9 cosmosex_raspbian > /dev/null 2> /dev/null
-killall -9 cosmosex_yocto    > /dev/null 2> /dev/null
+killall -9 cosmosex                  > /dev/null 2> /dev/null
+killall -9 cosmosex_raspbian         > /dev/null 2> /dev/null
+killall -9 cosmosex_raspbian_stretch > /dev/null 2> /dev/null
+killall -9 cosmosex_raspbian_jessie  > /dev/null 2> /dev/null
+killall -9 cosmosex_yocto            > /dev/null 2> /dev/null
