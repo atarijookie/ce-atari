4b1dd7cddeb7549fc29f4eafdf60189da85cd418
atarijookie <joo@kie.sk>
2020-09-05 09:25:18 +0200

    added RPi model detection, openOCD configs, 10M04 test file, modified update xilinx file for new fpga

--- ce_main_app/shellscripts/update_xilinx.sh
+++ ce_main_app/shellscripts/update_xilinx.sh
@@ -5,6 +5,7 @@ echo "XILINX firmware writing script."
 # initialize variables
 is36=0
 is72=0
+isFpga=0
 
 # test for XC9536 chip
 /ce/update/flash_xilinx /ce/update/test_xc9536xl.xsvf  > /dev/null 2> /dev/null
@@ -20,9 +21,16 @@ if [ "$?" -eq "0" ]; then
     is72=1
 fi
 
+# test for 10M04 FPGA chip
+/ce/update/flash_xilinx /ce/update/test_10m04.xsvf  > /dev/null 2> /dev/null
+
+if [ "$?" -eq "0" ]; then
+    isFpga=1
+fi
+
 # no chip detected? fail, quit
-if [ "$is36" -eq "0" ] && [ "$is72" -eq "0" ]; then
-    echo "No Xilinx type detected - this is invalid!"
+if [ "$is36" -eq "0" ] && [ "$is72" -eq "0" ] && [ "$isFpga" -eq "0" ]; then
+    echo "No CPLD / FPGA type detected - this is invalid!"
     exit
 fi
 
@@ -71,3 +79,20 @@ if [ "$is72" -eq "1" ]; then
     echo "Detected XC9572 chip but didn't write any firmware :("
 fi
 
+# for 10M04 FPGA - just burn the firmware
+if [ "$isFpga" -eq "1" ]; then
+    # write the 10M04 firmware
+    echo "Detected 10M04 chip, will write firmware"
+
+    # find out RPi model
+    rpi=$( /ce/whichrpi.sh )
+    echo "Detected RPi $rpi"
+    
+    # select openOCD config file based on detected RPi model
+    cfg="/ce/update/openocd_rpi$rpi.cfg"
+
+    openocd -f $cfg -c init -c "svf -quiet /ce/update/10m04.svf" -c shutdown
+    cp /ce/update/10m04.version /ce/update/10m04.current            # copy flashed version into current version file
+    ln -fs /ce/update/10m04.version /ce/update/xilinx_used.version  # xilinx_user.version will point to file from which we took the version, so when that file changes, we will know we need to update xilinx 
+    exit
+fi
