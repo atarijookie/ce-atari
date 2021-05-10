6ea07ce4c81339461fc47c319b4bfadaec206ec8
atarijookie <joo@kie.sk>
2021-05-10 12:18:03 +0200

    added newer stm32 flash tool, aded STM32H730 support to it, added CosmosEx specific code to it

---
+++ flash_stm32_2021/gpio.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#include "gpio.h"
+
+void spi_init(void);
+
+/* Notes:
+
+Pins states remain the same even after bcm2835_close() and even after prog termination.
+bcm2835_gpio_write doesn't influence SPI CS pins, they are controlled by SPI part of the library.
+*/
+
+bool gpio_open(void)
+{
+    if(geteuid() != 0) {
+        printf("\nThe bcm2835 library requires to be run as root, try again...\n");
+        return false;
+    }
+
+    // try to init the GPIO library
+    if (!bcm2835_init()) {
+        printf("\nbcm2835_init failed, can't use GPIO.\n");
+        return false;
+    }
+    
+    // pins for XILINX programming  
+    // outputs: TDI (GPIO3), TCK (GPIO4), TMS (GPIO17)
+    bcm2835_gpio_fsel(PIN_TDI,  BCM2835_GPIO_FSEL_OUTP);        // TDI
+    bcm2835_gpio_fsel(PIN_TMS,  BCM2835_GPIO_FSEL_OUTP);        // TMS
+    bcm2835_gpio_fsel(PIN_TCK,  BCM2835_GPIO_FSEL_OUTP);        // TCK
+    // inputs : TDO (GPIO2)
+    bcm2835_gpio_fsel(PIN_TDO,  BCM2835_GPIO_FSEL_INPT);        // TDO
+
+    bcm2835_gpio_write(PIN_TDI, LOW);
+    bcm2835_gpio_write(PIN_TMS, LOW);
+    bcm2835_gpio_write(PIN_TCK, LOW);   
+    
+    
+    // pins for both STM32 programming 
+    bcm2835_gpio_fsel(PIN_RESET_HANS,       BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(PIN_RESET_FRANZ,      BCM2835_GPIO_FSEL_OUTP);
+//  bcm2835_gpio_fsel(PIN_TXD,              BCM2835_GPIO_FSEL_OUTP);
+//  bcm2835_gpio_fsel(PIN_RXD,              BCM2835_GPIO_FSEL_INPT);                    
+    bcm2835_gpio_fsel(PIN_TX_SEL1N2,        BCM2835_GPIO_FSEL_OUTP);
+    bcm2835_gpio_fsel(PIN_BOOT0_FRANZ_HANS, BCM2835_GPIO_FSEL_OUTP);
+
+    bcm2835_gpio_write(PIN_TX_SEL1N2,           HIGH);
+    bcm2835_gpio_write(PIN_BOOT0_FRANZ_HANS,    LOW);       // BOOT0: L means boot from flash, H means boot the boot loader
+    
+    bcm2835_gpio_write(PIN_RESET_HANS,          HIGH);      // reset lines to RUN (not reset) state
+    bcm2835_gpio_write(PIN_RESET_FRANZ,         HIGH);
+
+    
+    // pins for communication with Franz and Hans
+    bcm2835_gpio_fsel(PIN_ATN_HANS,         BCM2835_GPIO_FSEL_INPT);            
+    bcm2835_gpio_fsel(PIN_ATN_FRANZ,        BCM2835_GPIO_FSEL_INPT);
+//  bcm2835_gpio_fsel(PIN_CS_HANS,          BCM2835_GPIO_FSEL_OUTP);
+//  bcm2835_gpio_fsel(PIN_CS_FRANZ,         BCM2835_GPIO_FSEL_OUTP);
+//  bcm2835_gpio_fsel(PIN_MOSI,             BCM2835_GPIO_FSEL_OUTP);
+//  bcm2835_gpio_fsel(PIN_MISO,             BCM2835_GPIO_FSEL_INPT);
+//  bcm2835_gpio_fsel(PIN_SCK,              BCM2835_GPIO_FSEL_OUTP);
+    
+    spi_init();
+    
+    return true;
+}
+
+void spi_init(void)
+{
+    bcm2835_spi_begin();
+    
+    bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);        // The default
+    bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);                     // The default
+
+    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_16);      // 16 =  64ns = 15.625MHz
+    // according to SCK: 0,5625 us / byte = ~ 1.77 MB/s, but according to CS it's more like 1.6 MB/s
+    
+    bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);        // the default
+    bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS1, LOW);        // the default
+
+    bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                        
+}
+
+void spi_tx_rx(int whichSpiCS, int count, BYTE *txBuf, BYTE *rxBuf)
+{
+    bcm2835_spi_chipSelect(whichSpiCS);
+    bcm2835_spi_transfernb((char *) txBuf, (char *) rxBuf, count); 
+}
+
+bool spi_atn(int whichSpiAtn)
+{
+    BYTE val;
+    
+    val = bcm2835_gpio_lev(whichSpiAtn); 
+    
+    return (val == HIGH);                   // returns true if pin is high, returns false if pin is low
+}
+
+void gpio_close(void)
+{
+    bcm2835_spi_end();          // end the SPI stuff here
+    
+    bcm2835_close();            // close the GPIO library and finish
+}
---
+++ flash_stm32_2021/init.c
@@ -0,0 +1,321 @@
+/*
+  stm32flash - Open Source ST STM32 flash program for *nix
+  Copyright (C) 2010 Geoffrey McRae <geoff@spacevs.com>
+  Copyright (C) 2013 Antonio Borneo <borneo.antonio@gmail.com>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+#include "compiler.h"
+#include "init.h"
+#include "serial.h"
+#include "stm32.h"
+#include "port.h"
+#include "utils.h"
+
+extern FILE *diag;
+
+struct gpio_list {
+	struct gpio_list *next;
+	int gpio;
+	int input; /* 1 if direction of gpio should be changed back to input. */
+	int exported; /* 0 if gpio should be unexported. */
+};
+
+#if defined(__linux__)
+static int write_to(const char *filename, const char *value)
+{
+	int fd, ret;
+
+	fd = open(filename, O_WRONLY);
+	if (fd < 0) {
+		fprintf(stderr, "Cannot open file \"%s\"\n", filename);
+		return 0;
+	}
+	ret = write(fd, value, strlen(value));
+	if (ret < 0) {
+		fprintf(stderr, "Error writing in file \"%s\"\n", filename);
+		close(fd);
+		return 0;
+	}
+	close(fd);
+	return 1;
+}
+
+static int read_from(const char *filename, char *buf, size_t len)
+{
+	int fd, ret;
+	size_t n = 0;
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0) {
+		fprintf(stderr, "Cannot open file \"%s\"\n", filename);
+		return 0;
+	}
+
+	do {
+		ret = read(fd, buf + n, len - n);
+		if (ret < 0) {
+			if (errno == EINTR || errno == EAGAIN || errno == EWOULDBLOCK)
+				continue; /* try again */
+			fprintf(stderr, "Error reading in file \"%s\"\n", filename);
+			close(fd);
+			return 0;
+		}
+		n += ret;
+	} while (n < len && ret);
+
+	close(fd);
+	return n;
+}
+
+static int drive_gpio(int n, int level, struct gpio_list **gpio_to_release)
+{
+	char num[16]; /* sized to carry MAX_INT */
+	char file[48]; /* sized to carry longest filename */
+	char dir;
+	struct stat buf;
+	struct gpio_list *new;
+	int ret;
+	int exported = 1;
+	int input = 0;
+
+	sprintf(file, "/sys/class/gpio/gpio%d/value", n);
+	ret = stat(file, &buf);
+	if (ret) {
+		/* file miss, GPIO not exported yet */
+		sprintf(num, "%d", n);
+		ret = write_to("/sys/class/gpio/export", num);
+		if (!ret)
+			return 0;
+		ret = stat(file, &buf);
+		if (ret) {
+			fprintf(stderr, "GPIO %d not available\n", n);
+			return 0;
+		}
+		/* give udevd a chance to set permissions */
+		if (access(file, W_OK))
+			usleep(10);
+		exported = 0;
+	}
+
+	sprintf(file, "/sys/class/gpio/gpio%d/direction", n);
+	ret = stat(file, &buf);
+	if (!ret)
+		if (read_from(file, &dir, sizeof(dir)))
+			if (dir == 'i')
+				input = 1;
+
+	if (exported == 0 || input == 1) {
+		new = (struct gpio_list *)malloc(sizeof(struct gpio_list));
+		if (new == NULL) {
+			fprintf(stderr, "Out of memory\n");
+			return 0;
+		}
+		new->gpio = n;
+		new->exported = exported;
+		new->input = input;
+		new->next = *gpio_to_release;
+		*gpio_to_release = new;
+	}
+
+	return write_to(file, level ? "high" : "low");
+}
+
+static int release_gpio(int n, int input, int exported)
+{
+	char num[16]; /* sized to carry MAX_INT */
+	char file[48]; /* sized to carry longest filename */
+
+	sprintf(num, "%d", n);
+	if (input) {
+		sprintf(file, "/sys/class/gpio/gpio%d/direction", n);
+		write_to(file, "in");
+	}
+	if (!exported)
+		write_to("/sys/class/gpio/unexport", num);
+
+	return 1;
+}
+#else
+static int drive_gpio(int __unused n, int __unused level,
+		      struct gpio_list __unused **gpio_to_release)
+{
+	fprintf(stderr, "GPIO control only available in Linux\n");
+	return 0;
+}
+#endif
+
+static int gpio_sequence(struct port_interface *port, const char *seq, size_t len_seq)
+{
+	struct gpio_list *gpio_to_release = NULL;
+#if defined(__linux__)
+	struct gpio_list *to_free;
+#endif
+	int ret = 0, level, gpio;
+	int sleep_time = 0;
+	int delimiter = 0;
+	const char *sig_str = NULL;
+	const char *s = seq;
+	size_t l = len_seq;
+
+	fprintf(diag, "\nGPIO sequence start\n");
+	while (ret == 0 && *s && l > 0) {
+		sig_str = NULL;
+		sleep_time = 0;
+		delimiter = 0;
+
+		if (*s == '-') {
+			level = 0;
+			s++;
+			l--;
+		} else
+			level = 1;
+
+		if (isdigit(*s)) {
+			gpio = atoi(s);
+			while (isdigit(*s)) {
+				s++;
+				l--;
+			}
+		} else if (l >= 3 && !strncmp(s, "rts", 3)) {
+			sig_str = s;
+			gpio = -GPIO_RTS;
+			s += 3;
+			l -= 3;
+		} else if (l >= 3 && !strncmp(s, "dtr", 3)) {
+			sig_str = s;
+			gpio = -GPIO_DTR;
+			s += 3;
+			l -= 3;
+		} else if (l >= 3 && !strncmp(s, "brk", 3)) {
+			sig_str = s;
+			gpio = -GPIO_BRK;
+			s += 3;
+			l -= 3;
+		} else if (*s && (l > 0)) {
+			delimiter = 1;
+			/* The ',' delimiter adds a 100 ms delay between signal toggles.
+			 * i.e -rts,dtr will reset rts, wait 100 ms, set dtr.
+			 *
+			 * The '&' delimiter adds no delay between signal toggles.
+			 * i.e -rts&dtr will reset rts and immediately set dtr.
+			 *
+			 * Example: -rts&dtr,,,rts,-dtr will reset rts and set dtr
+			 * without delay, then wait 300 ms, set rts, wait 100 ms, reset dtr.
+			 */
+			if (*s == ',') {
+				s++;
+				l--;
+				sleep_time = 100000;
+			} else if (*s == '&') {
+				s++;
+				l--;
+			} else {
+				fprintf(stderr, "Character \'%c\' is not a valid signal or separator\n", *s);
+				ret = 1;
+				break;
+			}
+		} else {
+			/* E.g. modifier without signal */
+			fprintf(stderr, "Invalid sequence %.*s\n", (int) len_seq, seq);
+			ret = 1;
+			break;
+		}
+
+		if (!delimiter) { /* actual gpio/port signal driving */
+			if (gpio < 0) {
+				gpio = -gpio;
+				fprintf(diag, " setting port signal %.3s to %i... ", sig_str, level);
+				ret = (port->gpio(port, gpio, level) != PORT_ERR_OK);
+				printStatus(diag, ret);
+			} else {
+				fprintf(diag, " setting gpio %i to %i... ", gpio, level);
+				ret = (drive_gpio(gpio, level, &gpio_to_release) != 1);
+				printStatus(diag, ret);
+			}
+		}
+
+		if (sleep_time) {
+			fprintf(diag, " delay %i us\n", sleep_time);
+			usleep(sleep_time);
+		}
+	}
+#if defined(__linux__)
+	while (gpio_to_release) {
+		release_gpio(gpio_to_release->gpio, gpio_to_release->input, gpio_to_release->exported);
+		to_free = gpio_to_release;
+		gpio_to_release = gpio_to_release->next;
+		free(to_free);
+	}
+#endif
+	fprintf(diag, "GPIO sequence end\n\n");
+	return ret;
+}
+
+static int gpio_bl_entry(struct port_interface *port, const char *seq)
+{
+	char *s;
+
+	if (seq == NULL || seq[0] == ':')
+		return 1;
+
+	s = strchr(seq, ':');
+	if (s == NULL)
+		return gpio_sequence(port, seq, strlen(seq));
+
+	return gpio_sequence(port, seq, s - seq);
+}
+
+int gpio_bl_exit(struct port_interface *port, const char *seq)
+{
+	char *s;
+
+	if (seq == NULL)
+		return 1;
+
+	s = strchr(seq, ':');
+	if (s == NULL || s[1] == '\0')
+		return 1;
+
+	return gpio_sequence(port, s + 1, strlen(s + 1));
+}
+
+int init_bl_entry(struct port_interface *port, const char *seq)
+{
+	if (seq)
+		return gpio_bl_entry(port, seq);
+
+	return 0;
+}
+
+int init_bl_exit(stm32_t *stm, struct port_interface *port, const char *seq)
+{
+	if (seq && strchr(seq, ':'))
+		return gpio_bl_exit(port, seq);
+
+	return stm32_reset_device(stm);
+}
