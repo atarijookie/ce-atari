a04b22f7e6971d766224b54d221a1c6d02d7bc18
atarijookie <joo@kie.sk>
2021-12-30 05:46:18 +0000

    added showing images on pages

--- ce_downloader/main.py
+++ ce_downloader/main.py
@@ -2,6 +2,7 @@ import os
 import re
 import threading
 import time
+import math
 import urllib3
 import urwid
 from setproctitle import setproctitle
@@ -15,7 +16,15 @@ LIST_OF_LISTS_LOCAL = PATH_TO_LISTS + LIST_OF_LISTS_FILE        # where the list
 setproctitle("ce_downloader")   # set process title
 list_of_lists = []      # list of dictionaries: {name, url, filename}
 list_index = 0          # index of list in list_of_lists which will be worked on
-list_of_items = []
+list_of_items = []      # list containing all the items from file (unfiltered)
+list_of_items_filtered = []     # filtered list of items (based on search string)
+pile_current_page = None        # urwid pile containing buttons for current page
+search_phrase = ""
+
+text_pages = None       # widget holding the text showing current and total pages
+page_current = 1        # currently shown page
+items_per_page = 20     # how many items per page we should show
+screen_width = 80       # should be 40 for ST low, 80 for ST mid
 
 # ----------------------
 
@@ -145,8 +154,9 @@ def download_lists(name):
 # ----------------------
 
 def load_list_from_csv(csv_filename):
-    global list_of_items
+    global list_of_items, list_of_items_filtered
     list_of_items = []
+    list_of_items_filtered = []
 
     # read whole file into memory, split to lines
     file = open(csv_filename, "r")
@@ -170,10 +180,19 @@ def load_list_from_csv(csv_filename):
 
         list_of_items.append(item)
 
+    # first the filtered list is the same as original list
+    list_of_items_filtered = list_of_items
+
 # ----------------------
 
-def get_list_page(item_start, items_count):
-    sublist = list_of_items[item_start : (item_start + items_count)]
+def get_list_for_current_page():
+    """ get one page from filtered list """
+    global list_of_items_filtered, items_per_page, page_current
+
+    page_current_zbi = (page_current - 1) if page_current > 1 else 0      # create zero-base index, but page_number could be 0
+    item_start = page_current_zbi * items_per_page
+
+    sublist = list_of_items_filtered[item_start : (item_start + items_per_page)]
     return sublist
 
 # ----------------------
@@ -188,17 +207,107 @@ def create_main_menu():
 
     for index, item in enumerate(list_of_lists):           # go through the list of lists, extract names, put them in the buttons
         button = urwid.Button(item['name'])
-        urwid.connect_signal(button, 'click', on_list_chosen, index)
+        urwid.connect_signal(button, 'click', on_show_selected_list, index)
         body.append(urwid.AttrMap(button, None, focus_map='reversed'))
 
     return urwid.ListBox(urwid.SimpleFocusListWalker(body))
 
-def on_list_chosen(button, choice):
+
+def get_item_btn_text(item):
+    """ for a specified item retrieve full or partial content """
+    global search_phrase, screen_width
+
+    item_content = item['content']          # start with whole content
+
+    if search_phrase:                       # got search phrase? show only part of the content that matches
+        content_parts = item['content'].split(',')      # split csv to list
+
+        for part in content_parts:          # go through list
+            part_lc = part.lower()          # create lowercase part
+            
+            if search_phrase in part_lc:    # if phrase found in lowercase part, use original part
+                item_content = part
+
+    content_len = screen_width - 4 - 14     # how long the content can be, when button sides and filename take some space
+    item_content = item_content[:content_len]   # get only part of the content
+
+    btn_text = "{:13} {}".format(item['filename'], item_content)    # format string as filename + content
+    return btn_text
+
+def get_current_page_buttons(as_tuple):
+    """ create buttons for the current page number as a list
+    as_tuple = False - for new pile, just list of buttons
+    as_tuple = True  - for existing pile, as a list of tuples of buttons
+    """
+    from urwid.widget import WEIGHT
+
+    global items_per_page
+    sublist = get_list_for_current_page()       # get items for this page
+
+    buttons = []
+
+    for item in sublist:                        # from the items create buttons
+        btn_text = get_item_btn_text(item)
+        btn = urwid.AttrMap(urwid.Button(btn_text), None, focus_map='reversed')
+        btn = (btn, (WEIGHT, 1)) if as_tuple else btn
+        buttons.append(btn)
+
+    padding_cnt = items_per_page - len(sublist) # calculate how much padding at the bottom we need
+
+    for i in range(padding_cnt):                # add all padding as needed
+        one = (urwid.Divider(), (WEIGHT, 1)) if as_tuple else urwid.Divider()
+        buttons.append(one)
+
+    return buttons
+
+
+def update_pile_with_current_buttons():
+    """ the pile_current_page might be already shown on the screen, 
+        in that case we need to update the .contents attribude with tuples """
+    global pile_current_page
+
+    current_page_buttons = get_current_page_buttons(as_tuple=(pile_current_page is not None))
+
+    if pile_current_page is None:       # no current pile? create pile from current page buttons
+        pile_current_page = urwid.Pile(current_page_buttons)
+    else:                               # we got the pile? update pile content with current page buttons
+        pile_current_page.contents = current_page_buttons
+
+
+def on_show_selected_list(button, choice):
     global list_index, list_of_lists
-    list_index = choice
+    global text_pages
+    list_index = choice     # store the chosen list index
+
+    page_current = 1        # start from page 1
 
     body = []
     body.append(urwid.Text(list_of_lists[list_index]['name'], align='center'))
+    
+    # add search edit line
+    widget_search = urwid.Edit("Search: ")
+    urwid.connect_signal(widget_search, 'change', search_changed)
+    body.append(urwid.AttrMap(widget_search, None, focus_map='reversed'))
+
+    # the next row will hold pages related stufff
+    pages_row = []
+
+    btn_prev = urwid.Button("prev")       # prev page button
+    urwid.connect_signal(btn_prev, 'click', btn_prev_clicked)
+    pages_row.append(urwid.AttrMap(btn_prev, None, focus_map='reversed'))
+
+    text_pages = urwid.Text("0/0", align='center')          # pages showing text
+    pages_row.append(text_pages)
+
+    btn_next = urwid.Button("next")       # next page button
+    urwid.connect_signal(btn_next, 'click', btn_next_clicked)
+    pages_row.append(urwid.AttrMap(btn_next, None, focus_map='reversed'))
+
+    btn_main = urwid.Button("back to menu")     # back to main menu button
+    urwid.connect_signal(btn_main, 'click', back_to_main_menu)
+    pages_row.append(urwid.AttrMap(btn_main, None, focus_map='reversed'))
+
+    body.append(urwid.Columns(pages_row))
 
     # try to load the list into memory
     error = None
@@ -214,23 +323,86 @@ def on_list_chosen(button, choice):
 
         btn = urwid.Button("Back")
         urwid.connect_signal(btn, 'click', back_to_main_menu)
-        body.append(btn)
+        body.append(urwid.AttrMap(btn, None, focus_map='reversed'))
 
         main.original_widget = urwid.Filler(urwid.Pile(body))
         return
 
-    # list loaded, show it
-    sublist = get_list_page(0, 25)
-    for item in sublist:
-        btn = urwid.Button(item['filename'])
-        body.append(btn)
+    global pile_current_page, search_phrase
+    search_phrase = ""                  # no search phrase yet
+    update_pile_with_current_buttons()  # first update pile with current buttons
+    body.append(pile_current_page)      # then add the pile with current page to body
 
-#    done = urwid.Button(u'Ok')
-#    urwid.connect_signal(done, 'click', exit_program)
+    show_page_text()                    # show the new page text
 
     main.original_widget = urwid.Filler(urwid.Pile(body))
 
+
+def search_changed(widget, search_string):
+    """ this gets called when search string changes """
+    global list_of_items, list_of_items_filtered, page_current, search_phrase
+
+    list_of_items_filtered = []
+    search_phrase = search_string.lower()           # search string to lower case before search
+
+    for item in list_of_items:                      # go through all items in list
+        content = item['content'].lower()           # content to lower case
+
+        if search_phrase in content:                # if search string in content found
+            list_of_items_filtered.append(item)     # append item
+
+    # now set the 1st page and show page text
+    page_current = 1
+    show_page_text()                    # show the new page text
+    update_pile_with_current_buttons()  # show the new buttons
+
+
+def get_total_pages():
+    global items_per_page, list_of_items_filtered
+
+    total_items = len(list_of_items_filtered)                            # how many items current list has
+    total_pages = math.ceil(total_items / items_per_page)       # how many pages current list has
+    return total_pages
+
+
+def show_page_text():
+    global text_pages, page_current
+    total_pages = get_total_pages()     # how many pages current list has
+    text_pages.set_text("{} / {}".format(page_current, total_pages))
+
+
+def on_page_change(change_direction):
+    global page_current
+
+    total_pages = get_total_pages()     # how many pages current list has
+
+    if total_pages == 0:                # no total pages? no current page
+        page_current = 1
+    else:                               # some total pages?
+        if change_direction > 0:                # to next page?
+            if page_current < total_pages:      # not on last page yet? increment
+                page_current += 1
+        else:                                   # to previous page?
+            if page_current > 1:                # not on 1st page yet? decrement
+                page_current -= 1
+
+    show_page_text()            # show the new page text
+
+
+def btn_prev_clicked(button):
+    """ on prev page clicked """
+    on_page_change(-1)
+    update_pile_with_current_buttons()
+
+
+def btn_next_clicked(button):
+    """ on next page clicked """
+    on_page_change(+1)
+    update_pile_with_current_buttons()
+
+
 def back_to_main_menu(button):
+    """ when we should return back to main menu """
     main.original_widget = urwid.Padding(create_main_menu(), left=2, right=2)
 
 def exit_program(button):
